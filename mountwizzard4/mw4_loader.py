41657960ab3975d62389f37f2f6747793e300bec
mworion <michael.wuertenberger@t-online.de>
2018-10-23 21:54:44 +0200

    changed all paths

--- mountwizzard4/mw4_loader.py
+++ mountwizzard4/mw4_loader.py
@@ -32,9 +32,9 @@ import numpy as np
 import PyQt5.QtCore
 import PyQt5.QtWidgets
 # local import
-import mw4_global
-import mw4_main
-import media.resources
+import mountwizzard4.mw4_global
+import mountwizzard4.mw4_main
+import mountwizzard4.media.resources
 
 
 class SplashScreen(PyQt5.QtCore.QObject):
@@ -156,18 +156,18 @@ def main():
     if getattr(sys, 'frozen', False):
         # we are running in a bundle
         # noinspection PyProtectedMember
-        mw4_global.bundle_dir = sys._MEIPASS
+        mountwizzard4.mw4_global.bundle_dir = sys._MEIPASS
         # on mac we have to change path of working directory
         if platform.system() == 'Darwin':
             os.chdir(os.path.dirname(sys.executable))
             os.chdir('..')
             os.chdir('..')
             os.chdir('..')
-            mw4_global.frozen = True
+            mountwizzard4.mw4_global.frozen = True
     else:
         # we are running in a normal Python environment
-        mw4_global.bundle_dir = os.path.dirname(os.path.abspath(__file__))
-        mw4_global.frozen = False
+        mountwizzard4.mw4_global.bundle_dir = os.path.dirname(os.path.abspath(__file__))
+        mountwizzard4.mw4_global.frozen = False
 
     # now instantiate the application from QApplication
     PyQt5.QtWidgets.QApplication.setAttribute(PyQt5.QtCore.Qt.AA_EnableHighDpiScaling, True)
@@ -180,10 +180,10 @@ def main():
     splash.showMessage('Start initialising')
     splash.setValue(0)
     # setting work dir:
-    mw4_global.work_dir = os.getcwd()
-    mw4_global.config_dir = mw4_global.work_dir + '/config'
-    mw4_global.data_dir = mw4_global.work_dir + '/data'
-    mw4_global.image_dir = mw4_global.work_dir + '/image'
+    mountwizzard4.mw4_global.work_dir = os.getcwd()
+    mountwizzard4.mw4_global.config_dir = mountwizzard4.mw4_global.work_dir + '/config'
+    mountwizzard4.mw4_global.data_dir = mountwizzard4.mw4_global.work_dir + '/data'
+    mountwizzard4.mw4_global.image_dir = mountwizzard4.mw4_global.work_dir + '/image'
     # now setup the logging environment
     splash.showMessage('Setup logging')
     splash.setValue(20)
@@ -208,21 +208,21 @@ def main():
     splash.setValue(30)
 
     # we put all the configurations and downloadable files for usage in the config dir
-    if not os.path.isdir(mw4_global.work_dir):
-        os.makedirs(mw4_global.work_dir)
-    if not os.path.isdir(mw4_global.config_dir):
-        os.makedirs(mw4_global.config_dir)
-    if not os.path.isdir(mw4_global.data_dir):
-        os.makedirs(mw4_global.data_dir)
-    if not os.path.isdir(mw4_global.image_dir):
-        os.makedirs(mw4_global.image_dir)
+    if not os.path.isdir(mountwizzard4.mw4_global.work_dir):
+        os.makedirs(mountwizzard4.mw4_global.work_dir)
+    if not os.path.isdir(mountwizzard4.mw4_global.config_dir):
+        os.makedirs(mountwizzard4.mw4_global.config_dir)
+    if not os.path.isdir(mountwizzard4.mw4_global.data_dir):
+        os.makedirs(mountwizzard4.mw4_global.data_dir)
+    if not os.path.isdir(mountwizzard4.mw4_global.image_dir):
+        os.makedirs(mountwizzard4.mw4_global.image_dir)
 
     # start logging with basic system data for information
     splash.showMessage('Logging environment')
     splash.setValue(40)
     logging.info('------------------------------------------------------------------------')
     logging.info('')
-    logging.info('MountWizzard {0} started !'.format(mw4_global.BUILD))
+    logging.info('MountWizzard {0} started !'.format(mountwizzard4.mw4_global.BUILD))
     logging.info('')
     logging.info('------------------------------------------------------------------------')
     logging.info('Platform         : {0}'.format(platform.system()))
@@ -248,9 +248,9 @@ def main():
             logging.info('IP addr.         : {0}'.format(hostname))
         logging.info('Hosts            : {0}'.format(hostSummary))
 
-    logging.info('Environment is   : {0}'.format('frozen' if mw4_global.frozen else 'live'))
-    logging.info('Actual workdir   : {0}'.format(mw4_global.work_dir))
-    logging.info('Bundle dir       : {0}'.format(mw4_global.bundle_dir))
+    logging.info('Environment is   : {0}'.format('frozen' if mountwizzard4.mw4_global.frozen else 'live'))
+    logging.info('Actual workdir   : {0}'.format(mountwizzard4.mw4_global.work_dir))
+    logging.info('Bundle dir       : {0}'.format(mountwizzard4.mw4_global.bundle_dir))
     logging.info('sys.argv[0]      : {0}'.format(sys.argv[0]))
     logging.info('os.path.basename : {0}'.format(os.path.basename(sys.argv[0])))
     logging.info('sys.executable   : {0}'.format(sys.executable))
@@ -261,13 +261,13 @@ def main():
     # checking if writable
     splash.showMessage('Checking work directories')
     splash.setValue(50)
-    if not os.access(mw4_global.work_dir, os.W_OK):
+    if not os.access(mountwizzard4.mw4_global.work_dir, os.W_OK):
         logging.error('no write access to workdir')
-    if not os.access(mw4_global.config_dir, os.W_OK):
+    if not os.access(mountwizzard4.mw4_global.config_dir, os.W_OK):
         logging.error('no write access to /config')
-    if not os.access(mw4_global.data_dir, os.W_OK):
+    if not os.access(mountwizzard4.mw4_global.data_dir, os.W_OK):
         logging.error('no write access to /data')
-    if not os.access(mw4_global.image_dir, os.W_OK):
+    if not os.access(mountwizzard4.mw4_global.image_dir, os.W_OK):
         logging.error('no write access to /image')
 
     # and finally starting the application
@@ -275,7 +275,7 @@ def main():
     splash.setValue(60)
     sys.excepthook = except_hook
     app.setWindowIcon(PyQt5.QtGui.QIcon(':/mw4.ico'))
-    mountApp = mw4_main.MountWizzard4()
+    mountApp = mountwizzard4.mw4_main.MountWizzard4()
     mountApp.mainW.show()
 
     # starting gui

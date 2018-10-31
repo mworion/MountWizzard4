6dc3eabe241d68995ea522a71f65c7460accd5a3
mworion <michael.wuertenberger@t-online.de>
2018-10-31 21:21:42 +0100

    optimized time ( reduced set in html in creator

--- mw4/gui/message_ui.py
+++ mw4/gui/message_ui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'message.ui'
 #
-# Created by: PyQt5 UI code generator 5.11.2
+# Created by: PyQt5 UI code generator 5.11.3
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -41,9 +41,20 @@ class Ui_MessageDialog(object):
         font.setBold(True)
         font.setWeight(75)
         self.message.setFont(font)
+        self.message.setAcceptDrops(False)
+        self.message.setLocale(QtCore.QLocale(QtCore.QLocale.English, QtCore.QLocale.UnitedStates))
         self.message.setFrameShadow(QtWidgets.QFrame.Plain)
         self.message.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustToContents)
+        self.message.setHtml("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'Arial\'; font-size:10pt; font-weight:600; font-style:normal;\">\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>")
         self.message.setAcceptRichText(False)
+        self.message.setTextInteractionFlags(QtCore.Qt.NoTextInteraction)
+        self.message.setPlaceholderText("")
+        self.message.setSearchPaths([])
+        self.message.setOpenLinks(False)
         self.message.setObjectName("message")
 
         self.retranslateUi(MessageDialog)
@@ -53,11 +64,6 @@ class Ui_MessageDialog(object):
         _translate = QtCore.QCoreApplication.translate
         MessageDialog.setWindowTitle(_translate("MessageDialog", "Messages"))
         self.message.setToolTip(_translate("MessageDialog", "Error Messages from Tool"))
-        self.message.setHtml(_translate("MessageDialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'Arial\'; font-size:10pt; font-weight:600; font-style:normal;\">\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:\'MS Shell Dlg 2\'; font-size:8pt; font-weight:400;\"><br /></p></body></html>"))
 
 
 if __name__ == "__main__":

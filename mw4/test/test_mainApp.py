51f90b154e96766c3906c60b5232427aab4a3a76
mworion <michael.wuertenberger@t-online.de>
2018-11-21 10:31:54 +0100

    update tests

--- mw4/test/test_mainApp.py
+++ mw4/test/test_mainApp.py
@@ -18,6 +18,8 @@
 #
 ###########################################################
 # standard libraries
+import os
+import shutil
 # external packages
 import PyQt5.QtCore
 # local import
@@ -28,7 +30,7 @@ test = PyQt5.QtWidgets.QApplication([])
 
 
 mwGlob = {'workDir': '.',
-          'configDir': './mw4/test/config/',
+          'configDir': './mw4/test/config',
           'build': 'test',
           }
 test_app = mainApp.MountWizzard4(mwGlob=mwGlob)
@@ -42,6 +44,9 @@ test_app = mainApp.MountWizzard4(mwGlob=mwGlob)
 
 def test_loadConfig_ok1():
     # new, no config
+    config = mwGlob['configDir'] + '/config'
+    if os.path.isdir(config):
+        os.remove(config)
     suc = test_app.loadConfig()
     assert suc
     assert '4.0' == test_app.config['version']
@@ -50,13 +55,17 @@ def test_loadConfig_ok1():
 
 
 def test_loadConfig_ok2():
-    # load ok config
-    configFilePath = './mw4/test/config/config_ok.cfg'
+    # load existing basic ok config without filepath
+    configTest = './mw4/test/config/config_ok.cfg'
+    config = mwGlob['configDir'] + '/config'
+    if os.path.isdir(config):
+        os.remove(config)
+    shutil.copy(configTest, config)
 
-    suc = test_app.loadConfig(configFilePath=configFilePath)
+    suc = test_app.loadConfig()
     assert suc
     assert '4.0' == test_app.config['version']
-    assert 'tester' == test_app.config['profileName']
+    assert 'config' == test_app.config['profileName']
 
 
 def test_loadConfig_ok3():

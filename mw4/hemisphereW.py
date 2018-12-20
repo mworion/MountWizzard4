5a720b7cc7aca228d73e1ff655393e175bfe09a3
mworion <michael.wuertenberger@t-online.de>
2018-12-20 18:03:39 +0100

    started with operation mode

--- mw4/hemisphereW.py
+++ mw4/hemisphereW.py
@@ -376,6 +376,39 @@ class HemisphereWindow(widget.MWidget):
         self.app.data.clearBuildP()
         self.drawHemisphere()
 
+    def setOperationMode(self, mode):
+        """
+
+        :param mode: operation mode as text
+        :return: success
+        """
+
+        if mode == 'normal':
+            # reset the settings
+            self.horizonMarker.set_marker('None')
+            self.horizonMarker.set_color('#006000')
+            self.pointsBuild.set_color('#00A000')
+            # self.starsAlignment.set_color('#C0C000')
+            # self.starsAlignment.set_markersize(6)
+            for i in range(0, len(self.starsAnnotate)):
+                self.starsAnnotate[i].set_color('#808080')
+            self.ui.hemisphere.stackUnder(self.ui.hemisphereMoving)
+        elif mode == 'edit_model':
+            self.pointsPlotBig.set_color('#FF00FF')
+            self.ui.hemisphereMoving.stackUnder(self.ui.hemisphere)
+        elif mode == 'edit_horizon':
+            self.maskPlotMarker.set_marker('o')
+            self.maskPlotMarker.set_color('#FF00FF')
+            self.ui.hemisphereMoving.stackUnder(self.ui.hemisphere)
+        elif mode == 'polar_align':
+            self.starsAlignment.set_color('#FFFF00')
+            self.starsAlignment.set_markersize(12)
+            for i in range(0, len(self.starsAnnotate)):
+                self.starsAnnotate[i].set_color('#F0F0F0')
+            self.ui.hemisphere.stackUnder(self.ui.hemisphereMoving)
+        self.drawCanvas()
+        return True
+
     def drawHemisphere(self):
         # shortening the references
         axes = self.hemisphereMat.figure.axes[0]

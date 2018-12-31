453c0d7ff5ecf02622cddcaf3d504c51814d05b6
mworion <michael.wuertenberger@t-online.de>
2018-12-31 16:28:08 +0100

    bugfix polar plot

--- mw4/mainApp.py
+++ mw4/mainApp.py
@@ -45,7 +45,7 @@ class MountWizzard4(PyQt5.QtCore.QObject):
 
     __all__ = ['MountWizzard4',
                ]
-    version = '0.4.dev1'
+    version = '0.4.dev2'
     logger = logging.getLogger(__name__)
 
     # central message and logging dispatching

b7b74ecd32836d1c37e7847ec79cead2209ac937
mworion <michael.wuertenberger@t-online.de>
2019-01-03 17:03:04 +0100

    made alt az text for parkPos persistent

--- mw4/gui/mainWmixin/tabSettParkPos.py
+++ mw4/gui/mainWmixin/tabSettParkPos.py
@@ -53,6 +53,12 @@ class SettParkPos(object):
         for i, textField in enumerate(self.posTexts):
             keyConfig = 'pos{0:1d}buttonText'.format(i)
             textField.setText(config.get(keyConfig, 'Park Pos {0:1d}'.format(i)))
+        for i, textField in enumerate(self.posAlt):
+            keyConfig = 'posAlt{0:1d}'.format(i)
+            textField.setText(config.get(keyConfig, '-'))
+        for i, textField in enumerate(self.posAz):
+            keyConfig = 'posAz{0:1d}'.format(i)
+            textField.setText(config.get(keyConfig, '-'))
         self.updateParkPosButtonText()
         return True
 
@@ -60,6 +66,12 @@ class SettParkPos(object):
         config = self.app.config['mainW']
         for i, textField in enumerate(self.posTexts):
             keyConfig = 'pos{0:1d}buttonText'.format(i)
+            config[keyConfig] = textField.text()
+        for i, textField in enumerate(self.posAlt):
+            keyConfig = 'posAlt{0:1d}'.format(i)
+            config[keyConfig] = textField.text()
+        for i, textField in enumerate(self.posAz):
+            keyConfig = 'posAz{0:1d}'.format(i)
             config[keyConfig] = textField.text()
         return True
 

eb138a6f9668d4f2ed57d6d4d215a0d545af3a2c
mworion <michael.wuertenberger@t-online.de>
2019-01-05 13:44:32 +0100

    update 0.5.dev1

--- mw4/mainApp.py
+++ mw4/mainApp.py
@@ -46,7 +46,7 @@ class MountWizzard4(PyQt5.QtCore.QObject):
 
     __all__ = ['MountWizzard4',
                ]
-    version = '0.5.dev0'
+    version = '0.5.dev1'
     logger = logging.getLogger(__name__)
 
     # central message and logging dispatching

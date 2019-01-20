84ace229fdc2c31eef1b428d75d6ebbf5eb3dbd2
mworion <michael.wuertenberger@t-online.de>
2019-01-20 22:05:11 +0100

    made class out of test for astrometry

--- mw4/astrometry/aNet.py
+++ mw4/astrometry/aNet.py
@@ -28,207 +28,205 @@ from astropy.io import fits
 # local imports
 
 
-logger = logging.getLogger()
+class Astrometry(object):
+    """
+    the class Astrometry inherits all information and handling of astrometry.net handling
 
-
-def getSystemPaths():
+        >>> astrometry = Astrometry()
     """
 
-    :return: path to binaries, path to config file
-    """
+    __all__ = ['Astrometry',
+               ]
 
-    if platform.system() == 'Darwin':
-        binPath = '/Applications/kstars.app/Contents/MacOS/astrometry/bin/'
-        cfgPath = binPath
-    elif platform.system() == 'Linux':
-        binPath = '/usr/bin/'
-        cfgPath = '/etc/'
-    else:
-        binPath = ''
-        cfgPath = ''
-    return binPath, cfgPath
+    version = '0.3'
+    logger = logging.getLogger(__name__)
 
+    def __init__(self):
+        if platform.system() == 'Darwin':
+            self.binPath = '/Applications/kstars.app/Contents/MacOS/astrometry/bin/'
+            self.cfgPath = '/Applications/kstars.app/Contents/MacOS/astrometry/bin/'
+        elif platform.system() == 'Linux':
+            self.binPath = '/usr/bin/'
+            self.cfgPath = '/etc/'
+        else:
+            self.binPath = ''
+            self.cfgPath = ''
 
-def convertToHMS(value):
-    """
+    @staticmethod
+    def convertToHMS(value):
+        """
 
-    :param value:
-    :return: converted value as string
-    """
-    value = value.split('.')
-    value = value[0].split(' ')
-    value = f'{value[0]}:{value[1]}:{value[2]}'
-    return value
+        :param value:
+        :return: converted value as string
+        """
+        value = value.split('.')
+        value = value[0].split(' ')
+        value = f'{value[0]}:{value[1]}:{value[2]}'
+        return value
 
+    @staticmethod
+    def convertToDMS(value):
+        """
 
-def convertToDMS(value):
-    """
+        :param value:
+        :return: converted value as string
+        """
+        value = value.split('.')
+        value = value[0].split(' ')
+        if int(value[0]) < 0:
+            sign = '-'
+        else:
+            sign = '+'
+        value = f'{sign}{value[0]}:{value[1]}:{value[2]}'
+        return value
 
-    :param value:
-    :return: converted value as string
-    """
-    value = value.split('.')
-    value = value[0].split(' ')
-    if int(value[0]) < 0:
-        sign = '-'
-    else:
-        sign = '+'
-    value = f'{sign}{value[0]}:{value[1]}:{value[2]}'
-    return value
+    def readCheckFitsData(self, fitsPath='', optionalScale=0):
+        """
+        readCheckFitsData reads the fits file with the image and tries to get some key fields out
+        of the header for preparing the solver. necessary data are
 
+            - 'SCALE': pixel scale in arcsec per pixel
+            - 'OBJCTRA' : ra position of the object in HMS format
+            - 'OBJCTDEC' : dec position of the object in DMS format
 
-def readCheckFitsData(fitsPath='', optionalScale=0):
-    """
-    readCheckFitsData reads the fits file with the image and tries to get some key fields out
-    of the header for preparing the solver. necessary data are
+        if OBJCTRA / OBJCTDEC is not readable or not present, we remove the parameters to get a
+        blind solve
 
-        - 'SCALE': pixel scale in arcsec per pixel
-        - 'OBJCTRA' : ra position of the object in HMS format
-        - 'OBJCTDEC' : dec position of the object in DMS format
+        :param fitsPath: fits file with image data
+        :param optionalScale: optional scaling used, when not scale parameter is in header
+        :return: options as string
+        """
 
-    if OBJCTRA / OBJCTDEC is not readable or not present, we remove the parameters to get a
-    blind solve
+        if not fitsPath:
+            return ''
 
-    :param fitsPath: fits file with image data
-    :param optionalScale: optional scaling used, when not scale parameter is in header
-    :return: options as string
-    """
+        with fits.open(fitsPath) as fitsHandle:
+            fitsHeader = fitsHandle[0].header
+            scale = fitsHeader.get('scale', '0')
+            ra = fitsHeader.get('OBJCTRA', '')
+            dec = fitsHeader.get('OBJCTDEC', '')
 
-    if not fitsPath:
-        return ''
+        if scale == 0:
+            scale = optionalScale
+        ra = self.convertToHMS(ra)
+        dec = self.convertToDMS(dec)
+        scaleLow = float(scale) * 0.9
+        scaleHigh = float(scale) * 1.1
 
-    with fits.open(fitsPath) as fitsHandle:
-        fitsHeader = fitsHandle[0].header
-        scale = fitsHeader.get('scale', '0')
-        ra = fitsHeader.get('OBJCTRA', '')
-        dec = fitsHeader.get('OBJCTDEC', '')
+        if not ra or not dec:
+            return ''
+        options = f' --scale-low {scaleLow} --scale-high {scaleHigh}'
+        options += f' --ra {ra} --dec {dec} --radius 1'
+        return options
 
-    if scale == 0:
-        scale = optionalScale
-    ra = convertToHMS(ra)
-    dec = convertToDMS(dec)
-    scaleLow = float(scale) * 0.9
-    scaleHigh = float(scale) * 1.1
+    @staticmethod
+    def addWCSDataToFits(fitsPath='', wcsPath=''):
+        """
+        addWCSDataToFits reads the fits file containing the wcs data output from solve-field
+        and embeds it to the given fits file with image
 
-    if not ra or not dec:
-        return ''
-    options = f' --scale-low {scaleLow} --scale-high {scaleHigh}'
-    options += f' --ra {ra} --dec {dec} --radius 1'
-    return options
+        :param fitsPath: path to the fits file, where the wcs header should be embedded
+        :param wcsPath: path to th fits file with wcs header
+        :return: success
+        """
 
+        if not fitsPath or not wcsPath:
+            return False
 
-def addWCSDataToFits(fitsPath='', wcsPath=''):
-    """
-    addWCSDataToFits reads the fits file containing the wcs data output from solve-field
-    and embeds it to the given fits file with image
-
-    :param fitsPath: path to the fits file, where the wcs header should be embedded
-    :param wcsPath: path to th fits file with wcs header
-    :return: success
-    """
-
-    if not fitsPath or not wcsPath:
-        return False
-
-    with fits.open(fitsPath) as fitsHandle:
-        fitsHeader = fitsHandle[0].header
-        with fits.open(wcsPath) as wcsHandle:
-            wcsHeader = wcsHandle[0].header
-            for key, value in wcsHeader.items():
-                if key.startswith('COMMENT'):
-                    continue
-                if key.startswith('HISTORY'):
-                    continue
-                fitsHeader[key] = value
-    return True
-
-
-def solve(binPath='', cfgPath='', fitsPath='', dataPath='', solveOptions=''):
-    """
-    Solve uses the astrometry.net solver capabilities. The intention is to use an offline
-    solving capability, so we need a installed instance. As we go multi platform and we
-    need to focus on MW function, we use the astrometry.net package which is distributed
-    with KStars / EKOS. Many thanks to them providing such a nice package.
-    As we go using astrometry.net we focus on the minimum feature set possible to omit
-    many of the installation and wrapping work to be done. So we only support solving of
-    FITS files, use no python environment for astrometry.net parts (as we could access these
-    via MW directly)
-
-    The base outside ideas of implementation come from astrometry.net itself and the
-    astrometry implementation from cloudmakers.eu (another nice package for MAC Astro
-    software)
-
-    We are using the following command line tools with options:
-
-    image2xy:
-        -O (overwriting file)
-        -o <output filename>
-        <fits file>
-
-    solve-field:
-        --overwrite
-        --no-plots
-        --no-remove-lines
-        --no-verify-uniformize
-        --sort-column FLUX  (sort for FLUX column)
-        --uniformize 0  (disabling it)
-        --config <config> (config file name, usually astrometry.cfg)
-        xy (the output file from image2xy)
-
-
-    :param binPath: directory with astrometry.net binaries
-    :param cfgPath: directory with astrometry.cfg file
-    :param fitsPath:  full path to fits file
-    :param dataPath: directory for temp data
-    :param solveOptions: option for solving
-    :return: suc, coords, wcsHeader
-    """
-    baseOptions = ' --overwrite --no-plots --no-remove-lines --no-verify-uniformize'
-    baseOptions += ' --uniformize 0 --sort-column FLUX --scale-units app'
-    baseOptions += ' --crpix-center --cpulimit 60'
-
-    command = binPath + 'image2xy'
-    xyPath = dataPath + 'temp.xy'
-    configPath = cfgPath + 'astrometry.cfg'
-    solvedPath = dataPath + 'temp.solved'
-    wcsPath = dataPath + 'temp.wcs'
-
-    image2xyOptions = f' -O -o {xyPath} {fitsPath}'
-    result = subprocess.getoutput(command + image2xyOptions)
-    logger.debug('image2xy: ', result)
-    if not result.startswith('simplexy: found'):
-        return false, None, None
-
-    extendedOptions = f' --config {configPath} {xyPath}'
-    options = baseOptions + extendedOptions + solveOptions
-    command = binPath + 'solve-field'
-
-    result = subprocess.getoutput(command + options)
-    logger.debug('solve-field: ', result)
-
-    if os.path.isfile(solvedPath) and os.path.isfile(wcsPath):
+        with fits.open(fitsPath) as fitsHandle:
+            fitsHeader = fitsHandle[0].header
+            with fits.open(wcsPath) as wcsHandle:
+                wcsHeader = wcsHandle[0].header
+                for key, value in wcsHeader.items():
+                    if key.startswith('COMMENT'):
+                        continue
+                    if key.startswith('HISTORY'):
+                        continue
+                    fitsHeader[key] = value
         return True
-    else:
-        return False
+
+    def solve(self, fitsPath='', dataPath='', solveOptions=''):
+        """
+        Solve uses the astrometry.net solver capabilities. The intention is to use an offline
+        solving capability, so we need a installed instance. As we go multi platform and we
+        need to focus on MW function, we use the astrometry.net package which is distributed
+        with KStars / EKOS. Many thanks to them providing such a nice package.
+        As we go using astrometry.net we focus on the minimum feature set possible to omit
+        many of the installation and wrapping work to be done. So we only support solving of
+        FITS files, use no python environment for astrometry.net parts (as we could access these
+        via MW directly)
+
+        The base outside ideas of implementation come from astrometry.net itself and the
+        astrometry implementation from cloudmakers.eu (another nice package for MAC Astro
+        software)
+
+        We are using the following command line tools with options:
+
+        image2xy:
+            -O (overwriting file)
+            -o <output filename>
+            <fits file>
+
+        solve-field:
+            --overwrite
+            --no-plots
+            --no-remove-lines
+            --no-verify-uniformize
+            --sort-column FLUX  (sort for FLUX column)
+            --uniformize 0  (disabling it)
+            --config <config> (config file name, usually astrometry.cfg)
+            xy (the output file from image2xy)
+
+
+        :param fitsPath:  full path to fits file
+        :param dataPath: directory for temp data
+        :param solveOptions: option for solving
+        :return: suc, coords, wcsHeader
+        """
+        baseOptions = ' --overwrite --no-plots --no-remove-lines --no-verify-uniformize'
+        baseOptions += ' --uniformize 0 --sort-column FLUX --scale-units app'
+        baseOptions += ' --crpix-center --cpulimit 60'
+
+        command = self.binPath + 'image2xy'
+        xyPath = dataPath + 'temp.xy'
+        configPath = self.cfgPath + 'astrometry.cfg'
+        solvedPath = dataPath + 'temp.solved'
+        wcsPath = dataPath + 'temp.wcs'
+
+        image2xyOptions = f' -O -o {xyPath} {fitsPath}'
+        result = subprocess.getoutput(command + image2xyOptions)
+        self.logger.debug('image2xy: ', result)
+        if not result.startswith('simplexy: found'):
+            return false, None, None
+
+        extendedOptions = f' --config {configPath} {xyPath}'
+        options = baseOptions + extendedOptions + solveOptions
+        command = self.binPath + 'solve-field'
+
+        result = subprocess.getoutput(command + options)
+        self.logger.debug('solve-field: ', result)
+
+        if os.path.isfile(solvedPath) and os.path.isfile(wcsPath):
+            return True
+        else:
+            return False
 
 
 if __name__ == "__main__":
     fitsFile = 'NGC7380.fits'
     # fitsFile = 'm51.fit'
 
-    binPath, cfgPath = getSystemPaths()
+    astrometry = Astrometry()
     pathToMW = '/Users/mw/PycharmProjects/MountWizzard4/'
     dataPath = pathToMW + 'data/'
     fitsPath = pathToMW + fitsFile
 
-    fitsOptions = readCheckFitsData(fitsPath=fitsPath)
+    fitsOptions = astrometry.readCheckFitsData(fitsPath=fitsPath)
 
-    result = solve(binPath=binPath,
-                   cfgPath=cfgPath,
-                   fitsPath=fitsPath,
-                   dataPath=dataPath,
-                   solveOptions=fitsOptions)
+    result = astrometry.solve(fitsPath=fitsPath,
+                              dataPath=dataPath,
+                              solveOptions=fitsOptions)
 
     wcsPath = dataPath + 'temp.wcs'
-    addWCSDataToFits(fitsPath=fitsPath,
-                     wcsPath=wcsPath)
+    astrometry.addWCSDataToFits(fitsPath=fitsPath,
+                                wcsPath=wcsPath)

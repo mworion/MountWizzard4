21cccbc4e747eb065001f64f8c60b374b0f76df2
mworion <michael.wuertenberger@t-online.de>
2019-02-13 18:34:26 +0100

    update tests

--- mw4/test/test_astrometryKstars.py
+++ mw4/test/test_astrometryKstars.py
@@ -30,6 +30,7 @@ from mw4.test.test_setupQt import setupQt
 app, spy, mwGlob, test = setupQt()
 
 tempDir = mwGlob['tempDir']
+threadPool = ''
 
 
 @pytest.fixture(autouse=True, scope='function')
@@ -45,7 +46,8 @@ def test_init_1():
     with mock.patch.object(platform,
                            'system',
                            return_value='Darwin'):
-        app = astrometryKstars.AstrometryKstars(tempDir=tempDir)
+        app = astrometryKstars.AstrometryKstars(tempDir=tempDir,
+                                                threadPool=threadPool)
         assert app.binPathSolveField == binSolve
         assert app.binPathImage2xy == binImage
         assert app.indexPath == index
@@ -59,7 +61,8 @@ def test_init_2():
     with mock.patch.object(platform,
                            'system',
                            return_value='Linux'):
-        app = astrometryKstars.AstrometryKstars(tempDir=tempDir)
+        app = astrometryKstars.AstrometryKstars(tempDir=tempDir,
+                                                threadPool=threadPool)
         assert app.binPathSolveField == binSolve
         assert app.binPathImage2xy == binImage
         assert app.indexPath == index
@@ -70,7 +73,8 @@ def test_init_3():
     with mock.patch.object(platform,
                            'system',
                            return_value='Windows'):
-        app = astrometryKstars.AstrometryKstars(tempDir=tempDir)
+        app = astrometryKstars.AstrometryKstars(tempDir=tempDir,
+                                                threadPool=threadPool)
         assert app.binPathSolveField == ''
         assert app.binPathImage2xy == ''
         assert app.indexPath == ''
@@ -78,111 +82,127 @@ def test_init_3():
 
 
 def test_convertToHMS_1():
-    app = astrometryKstars.AstrometryKstars(tempDir=tempDir)
+    app = astrometryKstars.AstrometryKstars(tempDir=tempDir,
+                                            threadPool=threadPool)
     value = 180.0
     ret = app.convertToHMS(value)
     assert ret == '12:00:00'
 
 
 def test_convertToHMS_2():
-    app = astrometryKstars.AstrometryKstars(tempDir=tempDir)
+    app = astrometryKstars.AstrometryKstars(tempDir=tempDir,
+                                            threadPool=threadPool)
     value = -180.0
     ret = app.convertToHMS(value)
     assert ret == '12:00:00'
 
 
 def test_convertToHMS_3():
-    app = astrometryKstars.AstrometryKstars(tempDir=tempDir)
+    app = astrometryKstars.AstrometryKstars(tempDir=tempDir,
+                                            threadPool=threadPool)
     value = '12 00 34'
     ret = app.convertToHMS(value)
     assert ret == '12:00:34'
 
 
 def test_convertToHMS_4():
-    app = astrometryKstars.AstrometryKstars(tempDir=tempDir)
+    app = astrometryKstars.AstrometryKstars(tempDir=tempDir,
+                                            threadPool=threadPool)
     value = '12 00 34 34'
     ret = app.convertToHMS(value)
     assert ret is None
 
 
 def test_convertToHMS_5():
-    app = astrometryKstars.AstrometryKstars(tempDir=tempDir)
+    app = astrometryKstars.AstrometryKstars(tempDir=tempDir,
+                                            threadPool=threadPool)
     value = '12:00:34'
     ret = app.convertToHMS(value)
     assert ret == '12:00:34'
 
 
 def test_convertToHMS_6():
-    app = astrometryKstars.AstrometryKstars(tempDir=tempDir)
+    app = astrometryKstars.AstrometryKstars(tempDir=tempDir,
+                                            threadPool=threadPool)
     value = '12:00:34.356'
     ret = app.convertToHMS(value)
     assert ret == '12:00:34'
 
 
 def test_convertToDMS_1():
-    app = astrometryKstars.AstrometryKstars(tempDir=tempDir)
+    app = astrometryKstars.AstrometryKstars(tempDir=tempDir,
+                                            threadPool=threadPool)
     value = 90.0
     ret = app.convertToDMS(value)
     assert ret == '+90:00:00'
 
 
 def test_convertToDMS_2():
-    app = astrometryKstars.AstrometryKstars(tempDir=tempDir)
+    app = astrometryKstars.AstrometryKstars(tempDir=tempDir,
+                                            threadPool=threadPool)
     value = -90.0
     ret = app.convertToDMS(value)
     assert ret == '-90:00:00'
 
 
 def test_convertToDMS_3():
-    app = astrometryKstars.AstrometryKstars(tempDir=tempDir)
+    app = astrometryKstars.AstrometryKstars(tempDir=tempDir,
+                                            threadPool=threadPool)
     value = '45 45 45'
     ret = app.convertToDMS(value)
     assert ret == '+45:45:45'
 
 
 def test_convertToDMS_4():
-    app = astrometryKstars.AstrometryKstars(tempDir=tempDir)
+    app = astrometryKstars.AstrometryKstars(tempDir=tempDir,
+                                            threadPool=threadPool)
     value = '45 45 45 50'
     ret = app.convertToDMS(value)
     assert ret is None
 
 
 def test_convertToDMS_5():
-    app = astrometryKstars.AstrometryKstars(tempDir=tempDir)
+    app = astrometryKstars.AstrometryKstars(tempDir=tempDir,
+                                            threadPool=threadPool)
     value = '45:45:45'
     ret = app.convertToDMS(value)
     assert ret == '+45:45:45'
 
 
 def test_convertToDMS_6():
-    app = astrometryKstars.AstrometryKstars(tempDir=tempDir)
+    app = astrometryKstars.AstrometryKstars(tempDir=tempDir,
+                                            threadPool=threadPool)
     value = '45 45 45.345'
     ret = app.convertToDMS(value)
     assert ret == '+45:45:45'
 
 
 def test_convertToDMS_7():
-    app = astrometryKstars.AstrometryKstars(tempDir=tempDir)
+    app = astrometryKstars.AstrometryKstars(tempDir=tempDir,
+                                            threadPool=threadPool)
     value = '++45 45 45'
     ret = app.convertToDMS(value)
     assert ret is None
 
 
 def test_convertToDMS_8():
-    app = astrometryKstars.AstrometryKstars(tempDir=tempDir)
+    app = astrometryKstars.AstrometryKstars(tempDir=tempDir,
+                                            threadPool=threadPool)
     value = '-45 45 45.345'
     ret = app.convertToDMS(value)
     assert ret == '-45:45:45'
 
 
 def test_checkAvailability_1():
-    app = astrometryKstars.AstrometryKstars(tempDir=tempDir)
+    app = astrometryKstars.AstrometryKstars(tempDir=tempDir,
+                                            threadPool=threadPool)
     suc = app.checkAvailability()
     assert suc
 
 
 def test_checkAvailability_2():
-    app = astrometryKstars.AstrometryKstars(tempDir=tempDir)
+    app = astrometryKstars.AstrometryKstars(tempDir=tempDir,
+                                            threadPool=threadPool)
     app.indexPath = ''
     app.binPathImage2xy = ''
     app.binPathSolveField = ''
@@ -191,7 +211,8 @@ def test_checkAvailability_2():
 
 
 def test_checkAvailability_3():
-    app = astrometryKstars.AstrometryKstars(tempDir=tempDir)
+    app = astrometryKstars.AstrometryKstars(tempDir=tempDir,
+                                            threadPool=threadPool)
     app.indexPath = ''
     app.binPathImage2xy = ''
     suc = app.checkAvailability()
@@ -199,17 +220,11 @@ def test_checkAvailability_3():
 
 
 def test_checkAvailability_4():
-    app = astrometryKstars.AstrometryKstars(tempDir=tempDir)
+    app = astrometryKstars.AstrometryKstars(tempDir=tempDir,
+                                            threadPool=threadPool)
     app.indexPath = ''
     suc = app.checkAvailability()
     assert not suc
 
-
-def test_solve_1():
-    fitsPath = './mw4/test/config/m51.fit'
-    # fitsPath = './NGC7635.fit'
-    app = astrometryKstars.AstrometryKstars(tempDir=tempDir)
-    solution = app.solve(fitsPath=fitsPath)
-    assert solution == [0, 0, 0, 0]
 
 

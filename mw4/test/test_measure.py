735a27fe63ee1f9403b470525adb483be1948884
mworion <michael.wuertenberger@t-online.de>
2019-03-08 08:44:53 +0100

    update tests

--- mw4/test/test_measure.py
+++ mw4/test/test_measure.py
@@ -29,18 +29,12 @@ from mw4.test.test_setupQt import setupQt
 app, spy, mwGlob, test = setupQt()
 
 
-@pytest.fixture(autouse=True, scope='function')
-def module_setup_teardown():
-    global app
-    app = mainApp.MountWizzard4(mwGlob=mwGlob)
-    yield
-
-
 def test_measureTask_1():
     app.mainW.ui.measureDevice.setCurrentIndex(1)
     app.environment.wDevice['local']['data'] = {}
     app.environment.wDevice['sqm']['data'] = {}
-    app.measure._measureTask()
+    suc = app.measure._measureTask()
+    assert suc
     assert app.measure.data['temp'].shape[0]
     assert app.measure.data['humidity'].shape[0]
     assert app.measure.data['press'].shape[0]
@@ -49,30 +43,43 @@ def test_measureTask_1():
 
 
 def test_measureTask_2():
+    app.mainW.ui.measureDevice.setCurrentIndex(0)
+    app.environment.wDevice['local']['data'] = {}
+    app.environment.wDevice['sqm']['data'] = {}
+    app.measure.data = {
+        'time': np.empty(shape=[0, 1], dtype='datetime64'),
+        'temp': np.empty(shape=[0, 1]),
+        'humidity': np.empty(shape=[0, 1]),
+        'press': np.empty(shape=[0, 1]),
+        'dewTemp': np.empty(shape=[0, 1]),
+        'sqr': np.empty(shape=[0, 1]),
+        'raJNow': np.empty(shape=[0, 1]),
+        'decJNow': np.empty(shape=[0, 1]),
+        'status': np.empty(shape=[0, 1]),
+    }
+    suc = app.measure._measureTask()
+    assert not suc
+    assert app.measure.data['temp'].shape[0] == 0
+    assert app.measure.data['humidity'].shape[0] == 0
+    assert app.measure.data['press'].shape[0] == 0
+    assert app.measure.data['dewTemp'].shape[0] == 0
+    assert app.measure.data['sqr'].shape[0] == 0
+
+
+def test_measureTask_3():
     app.mainW.ui.measureDevice.setCurrentIndex(1)
     app.environment.wDevice['local']['data']['WEATHER_TEMPERATURE'] = 10
     app.environment.wDevice['local']['data']['WEATHER_BAROMETER'] = 1000
     app.environment.wDevice['local']['data']['WEATHER_DEWPOINT'] = 10
     app.environment.wDevice['local']['data']['WEATHER_HUMIDITY'] = 10
     app.environment.wDevice['sqm']['data']['SKY_BRIGHTNESS'] = 19
-    app.measure._measureTask()
+    suc = app.measure._measureTask()
+    assert suc
     assert app.measure.data['temp'][0] == 10
     assert app.measure.data['humidity'][0] == 10
     assert app.measure.data['press'][0] == 1000
     assert app.measure.data['dewTemp'][0] == 10
     assert app.measure.data['sqr'][0] == 19
-
-
-def test_measureTask_3():
-    app.mainW.ui.measureDevice.setCurrentIndex(0)
-    app.environment.wDevice['local']['data'] = {}
-    app.environment.wDevice['sqm']['data'] = {}
-    app.measure._measureTask()
-    assert app.measure.data['temp'].shape[0] == 0
-    assert app.measure.data['humidity'].shape[0] == 0
-    assert app.measure.data['press'].shape[0] == 0
-    assert app.measure.data['dewTemp'].shape[0] == 0
-    assert app.measure.data['sqr'].shape[0] == 0
 
 
 def test_calculateReference_1():
@@ -139,9 +146,21 @@ def test_calculateReference_6():
     app.measure.decRef = 0
     app.mount.obsSite.raJNow = 1
     app.mount.obsSite.decJNow = 1
+    app.measure.data['status'] = np.array([0, 0, 0, 0, 0, 0, 0, 0])
     ra, dec = app.measure._calculateReference()
-    assert ra == 0
-    assert dec == 0
+    assert ra == 3600.0
+    assert dec == 3600.0
+
+
+def test_calculateReference_7():
+    app.measure.raRef = 0
+    app.measure.decRef = 0
+    app.mount.obsSite.raJNow = 1
+    app.mount.obsSite.decJNow = 1
+    app.measure.data['status'] = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+    ra, dec = app.measure._calculateReference()
+    assert ra == 0.0
+    assert dec == 0.0
 
 
 def test_checkSize_1():

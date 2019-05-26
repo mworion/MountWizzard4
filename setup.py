ee7950a09521bc9348dcf2bd561b8b4fe0b93a22
mworion <michael.wuertenberger@t-online.de>
2019-05-26 18:20:01 +0200

    moved from matplotlib 3.0.3 to 3.1

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     python_requires='>=3.7.2',
     install_requires=[
         'PyQt5==5.12',
-        'matplotlib==3.0.3',
+        'matplotlib==3.1',
         'requests==2.21.0',
         'requests_toolbelt==0.9.1',
         'numpy==1.16.2',

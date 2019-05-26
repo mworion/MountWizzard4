b87fc9d719f468133c56933cc3b14006f7951af2
mworion <michael.wuertenberger@t-online.de>
2019-05-26 17:30:25 +0200

    docs for named tuples

--- mw4/definitions.py
+++ mw4/definitions.py
@@ -19,11 +19,32 @@
 # standard libraries
 from collections import namedtuple
 
+# we are adding named tuples for the readability of the code
+# fist package is related to modeling procedure and defines the model points
+# and the necessary structures for storing the data, which is generated during
+# the model run
+
+# the model point itself
 Point = namedtuple('Point', 'altitude azimuth')
+
+# imaging parameters
 IParam = namedtuple('IParam', 'expTime binning subFrame fast')
+
+# modeling parameters
 MParam = namedtuple('MParam', 'number count path astrometry')
+
+# modeling generated data for programming the model
 MData = namedtuple('MData', 'raMJNow decMJNow raSJNow decSJNow sidereal julian pierside')
+
+# overall structure containing all of the above
 MPoint = namedtuple('MPoint', 'mParam iParam mPoint mData')
 
-Solution = namedtuple('Solution', 'success solve')
+
+# second cluster is for the astrometry stuff and solving data
+# during solving we extract some data from headers and store them
+
+# solving values
 Solve = namedtuple('Solve', 'raJ2000 decJ2000 angle scale error flipped')
+
+# overall structure which as well add the solve success part
+Solution = namedtuple('Solution', 'success solve')

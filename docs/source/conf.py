6b453e1c117db14298fdbb535fd303203a92c99c
mworion <michael.wuertenberger@t-online.de>
2019-11-23 22:03:07 +0100

    adding contents.rst

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ author = 'mworion'
 
 # The full version, including alpha/beta/rc tags
 release = '0.1'
-
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 

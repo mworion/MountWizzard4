410f297286edda4d3168afd56140a403b87ac0ba
mworion <michael.wuertenberger@t-online.de>
2019-12-30 14:38:19 +0100

    fixed skymeter tests

--- mw4/test/test_units/environment/test_skymeter.py
+++ mw4/test/test_units/environment/test_skymeter.py
@@ -36,114 +36,78 @@ def module_setup_teardown():
 
 
 def test_name():
+    app.skymeter.framework = 'indi'
     name = 'MBox'
     app.skymeter.name = name
     assert name == app.skymeter.name
+    assert app.skymeter.run['indi'].name == name
 
 
-def test_newDevice_1():
-    with mock.patch.object(app.skymeter.client,
-                           'isServerConnected',
-                           return_value=True):
-        with mock.patch.object(app.skymeter.client,
-                               'getDevice',
-                               return_value=1):
-            suc = app.skymeter.newDevice('test')
-            assert suc
-            assert app.skymeter.device is None
-
-
-def test_newDevice_2():
-    app.skymeter.name = 'Test'
-    with mock.patch.object(app.skymeter.client,
-                           'isServerConnected',
-                           return_value=True):
-        with mock.patch.object(app.skymeter.client,
-                               'getDevice',
-                               return_value=1):
-            suc = app.skymeter.newDevice('Test')
-            assert suc
-            assert app.skymeter.device == 1
-
-
-def test_removeDevice_1():
-    app.skymeter.name = 'Test'
-    with mock.patch.object(app.skymeter.client,
-                           'isServerConnected',
-                           return_value=True):
-        suc = app.skymeter.removeDevice('Test')
-        assert suc
-        assert app.skymeter.device is None
-        assert app.skymeter.data == {}
+def test_host():
+    app.skymeter.framework = 'indi'
+    host = ('localhost', 7624)
+    app.skymeter.host = host
+    assert app.skymeter.host == host
+    assert app.skymeter.run['indi'].host == host
+    assert app.skymeter.run['indi'].client.host == host
 
 
 def test_startCommunication_1():
+    app.skymeter.framework = 'indi'
     app.skymeter.name = ''
-    with mock.patch.object(app.skymeter.client,
+    with mock.patch.object(app.skymeter.run['indi'].client,
                            'connectServer',
                            return_value=False):
-        suc = app.skymeter.startCommunication()
+        suc = app.skymeter.run['indi'].startCommunication()
         assert not suc
 
 
-def test_setUpdateRate_1():
-    app.skymeter.name = 'test'
-    suc = app.skymeter.setUpdateConfig('false')
-    assert not suc
-
-
-def test_setUpdateRate_2():
-    app.skymeter.name = 'test'
-    app.skymeter.device = None
-    suc = app.skymeter.setUpdateConfig('test')
-    assert not suc
-
-
-def test_setUpdateRate_3():
-    class Test:
-        @staticmethod
-        def getNumber(test):
-            return {}
-    app.skymeter.name = 'test'
-    app.skymeter.device = Test()
-    suc = app.skymeter.setUpdateConfig('test')
-    assert not suc
-
-
-def test_setUpdateRate_4():
-    class Test:
-        @staticmethod
-        def getNumber(test):
-            return {'PERIOD': 1}
-    app.skymeter.name = 'test'
-    app.skymeter.device = Test()
-    suc = app.skymeter.setUpdateConfig('test')
-    assert not suc
-
-
-def test_setUpdateRate_5():
-    class Test:
-        @staticmethod
-        def getNumber(test):
-            return {'PERIOD': 10}
-    app.skymeter.name = 'test'
-    app.skymeter.device = Test()
-    with mock.patch.object(app.skymeter.client,
-                           'sendNewNumber',
+def test_startCommunication_2():
+    app.skymeter.framework = ''
+    app.skymeter.name = ''
+    with mock.patch.object(app.skymeter.run['indi'].client,
+                           'connectServer',
                            return_value=False):
-        suc = app.skymeter.setUpdateConfig('test')
+        suc = app.skymeter.run['indi'].startCommunication()
         assert not suc
 
 
-def test_setUpdateRate_6():
-    class Test:
-        @staticmethod
-        def getNumber(test):
-            return {'PERIOD': 10}
-    app.skymeter.name = 'test'
-    app.skymeter.device = Test()
-    with mock.patch.object(app.skymeter.client,
-                           'sendNewNumber',
-                           return_value=True):
-        suc = app.skymeter.setUpdateConfig('test')
-        assert suc
+def test_startCommunication_3():
+    app.skymeter.framework = 'indi'
+    app.skymeter.name = 'MBox'
+    with mock.patch.object(app.skymeter.run['indi'].client,
+                           'connectServer',
+                           return_value=False):
+        suc = app.skymeter.run['indi'].startCommunication()
+        assert not suc
+
+
+def test_stopCommunication_1():
+    app.skymeter.framework = 'indi'
+    app.skymeter.name = ''
+    with mock.patch.object(app.skymeter.run['indi'].client,
+                           'disconnectServer',
+                           return_value=False):
+        suc = app.skymeter.run['indi'].stopCommunication()
+        assert not suc
+
+
+def test_stopCommunication_2():
+    app.skymeter.framework = ''
+    app.skymeter.name = ''
+    with mock.patch.object(app.skymeter.run['indi'].client,
+                           'disconnectServer',
+                           return_value=False):
+        suc = app.skymeter.run['indi'].stopCommunication()
+        assert not suc
+
+
+def test_stopCommunication_3():
+    app.skymeter.framework = 'indi'
+    app.skymeter.name = 'MBox'
+    with mock.patch.object(app.skymeter.run['indi'].client,
+                           'disconnectServer',
+                           return_value=False):
+        suc = app.skymeter.run['indi'].stopCommunication()
+        assert not suc
+

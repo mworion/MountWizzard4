ebf4161013137c7eefd07fa60fb2a6d18cd5c490
mworion <michael.wuertenberger@t-online.de>
2020-02-26 18:06:06 +0100

    added test

---
+++ mw4/test/test_units/base/test_definitions.py
@@ -0,0 +1,35 @@
+############################################################
+# -*- coding: utf-8 -*-
+#
+#       #   #  #   #   #    #
+#      ##  ##  #  ##  #    #
+#     # # # #  # # # #    #  #
+#    #  ##  #  ##  ##    ######
+#   #   #   #  #   #       #
+#
+# Python-based Tool for interaction with the 10micron mounts
+# GUI with PyQT5 for python
+# Python  v3.7.5
+#
+# Michael Würtenberger
+# (c) 2019
+#
+# Licence APL2.0
+#
+###########################################################
+# standard libraries
+from unittest import mock
+import time
+import pytest
+from collections import namedtuple
+# external packages
+
+# local import
+
+
+def test_definitions():
+    from mw4.base.definitions import Point
+
+    p = Point(altitude=0, azimuth=0)
+
+    assert type(p) == Point

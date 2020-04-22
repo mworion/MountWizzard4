ba938dd5b1e3e94d53da999ca4c22dfec38416f0
mworion <michael@wuertenberger.org>
2020-04-22 22:42:53 +0200

    bugfix error angle type

--- mw4/gui/mainWmixin/tabModel.py
+++ mw4/gui/mainWmixin/tabModel.py
@@ -626,7 +626,7 @@ class Model(object):
             mPoint['errorRMS'] = starList[i].errorRMS
             mPoint['errorRA'] = starList[i].errorRA()
             mPoint['errorDEC'] = starList[i].errorDEC()
-            mPoint['errorAngle'] = starList[i].errorAngle
+            mPoint['errorAngle'] = starList[i].errorAngle.degrees
             mPoint['errorIndex'] = starList[i].number
 
         return True

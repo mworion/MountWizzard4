8da93fb55e6983632b02c853e56c6398c1028462
mworion <michael@wuertenberger.org>
2020-04-26 20:20:39 +0200

    update version in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,8 +22,8 @@ copyright = '2020, mworion'
 author = 'mworion'
 
 # The full version, including alpha/beta/rc tags
-version = '0.150.8'
-version = '0.150.8'
+version = '0.150.9'
+version = '0.150.9'
 master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------

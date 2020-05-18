7c437617f638fb88b735d48d642cdd7d6ed41325
mworion <michael@wuertenberger.org>
2020-05-18 17:50:45 +0200

    merge ascom to master

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,8 +22,8 @@ copyright = '2020, mworion'
 author = 'mworion'
 
 # The full version, including alpha/beta/rc tags
-version = '0.150.15'
-version = '0.150.15'
+version = '0.150.16b1'
+version = '0.150.16b1'
 master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------

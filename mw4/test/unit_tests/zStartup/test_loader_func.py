65873732da90e4f12f130e968cc09f6e525f3c8e
mworion <orion.49m@googlemail.com>
2020-05-21 17:00:57 +0200

    correcting path for test

--- mw4/test/unit_tests/zStartup/test_loader_func.py
+++ mw4/test/unit_tests/zStartup/test_loader_func.py
@@ -119,7 +119,7 @@ def test_extractDataFiles_3(qtbot):
 
 def test_extractDataFiles_4(qtbot):
     mwGlob = dict()
-    mwGlob['dataDir'] = os.getcwd() + '/mw4/test/data'
+    mwGlob['dataDir'] = os.path.abspath(os.getcwd() + '/mw4/test/data')
     with mock.patch.object(os.path,
                            'isfile',
                            return_value=False):

d9a41248aa2120e02d2d843a01295951eb07d82f
mworion <michael@wuertenberger.org>
2020-05-26 15:58:48 +0200

    update

--- gists/angular.py
+++ gists/angular.py
@@ -1,7 +1,33 @@
 from mountcontrol.mount import Mount
 
+print("Importing Python Packages - please wait.")
+
+import itertools
+import threading
+import time
+import sys
+
+
+#here is the animation
+def animate():
+    for c in itertools.cycle(['|', '/', '-', '\\']):
+        if done:
+            break
+        sys.stdout.write('\rThinking ' + c)
+        sys.stdout.flush()
+        time.sleep(0.1)
+    # sys.stdout.write('\nDone!     \n')
+    # print('\nDone!     \n')
+
+done = False
+t = threading.Thread(target=animate, daemon=True)
+t.start()
+
+time.sleep(2)
+
 
 def main():
+    return
     m = Mount(host=('192.168.2.15', 3492),
               MAC='00.c0.08.87.35.db',
               pathToData='.',

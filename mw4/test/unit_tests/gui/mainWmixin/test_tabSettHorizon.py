e0af9b1ad2de2efd49d53443e05b3b0747d2b6f0
mworion <michael@wuertenberger.org>
2020-06-05 09:18:53 +0200

    test update

--- mw4/test/unit_tests/gui/mainWmixin/test_tabSettHorizon.py
+++ mw4/test/unit_tests/gui/mainWmixin/test_tabSettHorizon.py
@@ -54,6 +54,7 @@ def module_setup_teardown(qtbot):
         mount = Mount(host='localhost', MAC='00:00:00:00:00:00', expire=False, verbose=False,
                       pathToData='mw4/test/data')
         update1s = pyqtSignal()
+        redrawHemisphere = pyqtSignal()
         message = pyqtSignal(str, int)
         mwGlob = {'configDir': 'mw4/test/config'}
         data = DataPoint(app=Test1(), configDir='mw4/test/config')

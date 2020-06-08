f0856d254cf6c26e96df069433b9de9030446465
mworion <michael@wuertenberger.org>
2020-06-08 09:23:25 +0200

    adding almanac tab in doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,8 +22,8 @@ copyright = '2020, mworion'
 author = 'mworion'
 
 # The full version, including alpha/beta/rc tags
-version = '0.150.25b11'
-version = '0.150.25b11'
+version = '0.150.25b12'
+version = '0.150.25b12'
 master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------

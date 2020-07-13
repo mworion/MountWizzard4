1a127f6dff08c1eb6de54d556b29340fe9f83a08
mworion <michael@wuertenberger.org>
2020-07-13 18:42:40 +0200

    refactor tools and materials to simulator package

---
+++ mw4/gui/simulator/tools.py
@@ -0,0 +1,104 @@
+############################################################
+# -*- coding: utf-8 -*-
+#
+#       #   #  #   #   #    #
+#      ##  ##  #  ##  #    #
+#     # # # #  # # # #    #  #
+#    #  ##  #  ##  ##    ######
+#   #   #   #  #   #       #
+#
+# Python-based Tool for interaction with the 10micron mounts
+# GUI with PyQT5 for python
+#
+# written in python3 , (c) 2019, 2020 by mworion
+#
+# Licence APL2.0
+#
+###########################################################
+# standard libraries
+
+# external packages
+from PyQt5.QtCore import QUrl
+from PyQt5.QtGui import QVector3D, QFont
+from PyQt5.Qt3DExtras import QCuboidMesh, QSphereMesh
+from PyQt5.Qt3DExtras import QExtrudedTextMesh, QCylinderMesh
+from PyQt5.Qt3DRender import QMesh
+from PyQt5.Qt3DCore import QEntity, QTransform
+
+# local import
+
+
+def linkModel(model, name, rootEntity):
+    """
+
+    :param model:
+    :param name:
+    :param rootEntity:
+    :return:
+    """
+
+    currMod = model[name]
+
+    parent = currMod.get('parent', None)
+    if parent and model.get(parent, None):
+        currMod['e'] = QEntity(model[parent]['e'])
+    else:
+        currMod['e'] = QEntity(rootEntity)
+
+    visible = currMod.get('visible', True)
+    currMod['e'].setEnabled(visible)
+
+    source = currMod.get('source', None)
+    if source:
+        if isinstance(source, str):
+            mesh = QMesh()
+            mesh.setSource(QUrl(f'qrc:/model3D/{source}'))
+        elif isinstance(source[0], QCuboidMesh):
+            mesh = source[0]
+            mesh.setXExtent(source[1])
+            mesh.setYExtent(source[2])
+            mesh.setZExtent(source[3])
+        elif isinstance(source[0], QSphereMesh):
+            mesh = source[0]
+            mesh.setRadius(source[1])
+            mesh.setRings(source[2])
+            mesh.setSlices(source[3])
+        elif isinstance(source[0], QCylinderMesh):
+            mesh = source[0]
+            mesh.setLength(source[1])
+            mesh.setRadius(source[2])
+            mesh.setRings(source[3])
+            mesh.setSlices(source[4])
+        elif isinstance(source[0], QExtrudedTextMesh):
+            mesh = source[0]
+            mesh.setDepth(source[1])
+            mesh.setFont(QFont())
+            mesh.setText(source[3])
+
+        currMod['e'].addComponent(mesh)
+        currMod['m'] = mesh
+
+    trans = currMod.get('trans', None)
+    rot = currMod.get('rot', None)
+    scale = currMod.get('scale', None)
+    if trans or rot or scale:
+        transform = QTransform()
+
+        if trans and isinstance(trans, list) and len(trans) == 3:
+            transform.setTranslation(QVector3D(*trans))
+
+        if rot and isinstance(rot, list) and len(rot) == 3:
+            transform.setRotationX(rot[0])
+            transform.setRotationY(rot[1])
+            transform.setRotationZ(rot[2])
+
+        if scale and isinstance(scale, list) and len(scale) == 3:
+            transform.setScale3D(QVector3D(*scale))
+
+        currMod['e'].addComponent(transform)
+        currMod['t'] = transform
+
+    mat = currMod.get('mat', None)
+    if mat:
+        currMod['mat'] = mat
+        currMod['e'].addComponent(mat)
--- mw4/gui/simulatorW.py
+++ mw4/gui/simulatorW.py
@@ -19,20 +19,20 @@
 
 # external packages
 import numpy as np
-from PyQt5.QtCore import QUrl
 from PyQt5.QtGui import QColor, QFont
 from PyQt5.QtGui import QVector3D
 from PyQt5.QtWidgets import QWidget
 from PyQt5.Qt3DExtras import Qt3DWindow, QCuboidMesh, QSphereMesh
 from PyQt5.Qt3DExtras import QOrbitCameraController, QExtrudedTextMesh, QCylinderMesh
-from PyQt5.Qt3DRender import QMesh, QPointLight
+from PyQt5.Qt3DRender import QPointLight
 from PyQt5.Qt3DCore import QEntity, QTransform
 
 # local import
 from mw4.base import transform
 from mw4.gui import widget
 from mw4.gui.widgets import simulator_ui
-from mw4.gui.simulatorMaterials import Materials
+from gui.simulator.materials import Materials
+from gui.simulator import tools
 
 
 class SimulatorWindow(widget.MWidget):
@@ -340,82 +340,6 @@ class SimulatorWindow(widget.MWidget):
 
         return True
 
-    @staticmethod
-    def linkModel(model, name, rootEntity):
-        """
-
-        :param model:
-        :param name:
-        :param rootEntity:
-        :return:
-        """
-
-        currMod = model[name]
-
-        parent = currMod.get('parent', None)
-        if parent and model.get(parent, None):
-            currMod['e'] = QEntity(model[parent]['e'])
-        else:
-            currMod['e'] = QEntity(rootEntity)
-
-        visible = currMod.get('visible', True)
-        currMod['e'].setEnabled(visible)
-
-        source = currMod.get('source', None)
-        if source:
-            if isinstance(source, str):
-                mesh = QMesh()
-                mesh.setSource(QUrl(f'qrc:/model3D/{source}'))
-            elif isinstance(source[0], QCuboidMesh):
-                mesh = source[0]
-                mesh.setXExtent(source[1])
-                mesh.setYExtent(source[2])
-                mesh.setZExtent(source[3])
-            elif isinstance(source[0], QSphereMesh):
-                mesh = source[0]
-                mesh.setRadius(source[1])
-                mesh.setRings(source[2])
-                mesh.setSlices(source[3])
-            elif isinstance(source[0], QCylinderMesh):
-                mesh = source[0]
-                mesh.setLength(source[1])
-                mesh.setRadius(source[2])
-                mesh.setRings(source[3])
-                mesh.setSlices(source[4])
-            elif isinstance(source[0], QExtrudedTextMesh):
-                mesh = source[0]
-                mesh.setDepth(source[1])
-                mesh.setFont(QFont())
-                mesh.setText(source[3])
-
-            currMod['e'].addComponent(mesh)
-            currMod['m'] = mesh
-
-        trans = currMod.get('trans', None)
-        rot = currMod.get('rot', None)
-        scale = currMod.get('scale', None)
-        if trans or rot or scale:
-            transform = QTransform()
-
-            if trans and isinstance(trans, list) and len(trans) == 3:
-                transform.setTranslation(QVector3D(*trans))
-
-            if rot and isinstance(rot, list) and len(rot) == 3:
-                transform.setRotationX(rot[0])
-                transform.setRotationY(rot[1])
-                transform.setRotationZ(rot[2])
-
-            if scale and isinstance(scale, list) and len(scale) == 3:
-                transform.setScale3D(QVector3D(*scale))
-
-            currMod['e'].addComponent(transform)
-            currMod['t'] = transform
-
-        mat = currMod.get('mat', None)
-        if mat:
-            currMod['mat'] = mat
-            currMod['e'].addComponent(mat)
-
     def createOTA(self, rootEntity):
         """
         first transformation is from
@@ -573,7 +497,7 @@ class SimulatorWindow(widget.MWidget):
         }
 
         for name in self.model:
-            self.linkModel(self.model, name, rootEntity)
+            tools.linkModel(self.model, name, rootEntity)
 
     def createWorld(self, rootEntity):
         """
@@ -648,7 +572,7 @@ class SimulatorWindow(widget.MWidget):
         }
 
         for name in self.world:
-            self.linkModel(self.world, name, rootEntity)
+            tools.linkModel(self.world, name, rootEntity)
 
     @staticmethod
     def createLine(rEntity, dx, dy, dz):
@@ -1085,7 +1009,7 @@ class SimulatorWindow(widget.MWidget):
 
         if 'ABS_DOME_POSITION.DOME_ABSOLUTE_POSITION' in self.app.dome.data:
             az = self.app.dome.data['ABS_DOME_POSITION.DOME_ABSOLUTE_POSITION']
-            self.world['domeSphere']['t'].setRotationZ(az)
+            self.world['domeSphere']['t'].setRotationZ(-az)
 
         if 'DOME_SHUTTER.SHUTTER_OPEN' in self.app.dome.data:
             radius = self.app.mainW.ui.domeRadius.value() * 1000

bugfix changing degrees to radians

mw4/gui/widget.py
@@ -21,6 +21,7 @@ import platform
 import os
 
 # external packages
+import numpy as np
 import PyQt5.QtWidgets
 import PyQt5.QtGui
 import PyQt5.QtCore
@@ -531,7 +532,7 @@ class MWidget(PyQt5.QtWidgets.QWidget, styles.MWStyles):
         axe.set_rlabel_position(45)
         axe.set_theta_direction(-1)
         # ticks have to be set before labels to be sure to have them positioned correctly
-        axe.set_xticks([0, 45, 90, 135, 180, 225, 270, 315])
+        axe.set_xticks(np.radians([0, 45, 90, 135, 180, 225, 270, 315]))
         axe.set_xticklabels(['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'])
 
         return axe, fig
01b68db11185fc504ac6b473de5f007edc04a4da
mworion <michael@wuertenberger.org>
2020-09-06 13:54:28 +0200

    lgtm correction

--- mw4/gui/extWindows/devicePopupW.py
+++ mw4/gui/extWindows/devicePopupW.py
@@ -20,7 +20,7 @@ import platform
 
 # external packages
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QDialog, QWidget, QListView, QComboBox, QLineEdit
+from PyQt5.QtWidgets import QWidget, QListView, QComboBox, QLineEdit
 from PyQt5.QtWidgets import QCheckBox, QDoubleSpinBox
 
 if platform.system() == 'Windows':

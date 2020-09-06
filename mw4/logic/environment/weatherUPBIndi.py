b76cb9ae9d1433ece63951d13a29985172832bf0
mworion <michael@wuertenberger.org>
2020-09-06 11:25:52 +0200

    splitting UPB into obs and power

---
+++ mw4/logic/environment/weatherUPBIndi.py
@@ -0,0 +1,79 @@
+############################################################
+# -*- coding: utf-8 -*-
+#
+#       #   #  #   #   #    #
+#      ##  ##  #  ##  #    #
+#     # # # #  # # # #    #  #
+#    #  ##  #  ##  ##    ######
+#   #   #   #  #   #       #
+#
+# Python-based Tool for interaction with the 10micron mounts
+# GUI with PyQT5 for python
+#
+# written in python 3, (c) 2019, 2020 by mworion
+#
+# Licence APL2.0
+#
+###########################################################
+# standard libraries
+import logging
+# external packages
+# local imports
+from base.loggerMW import CustomLogger
+from base.indiClass import IndiClass
+
+
+class WeatherUPBIndi(IndiClass):
+    """
+    the class PegasusUPBIndi inherits all information and handling of the PegasusUPB device
+
+        >>> power = WeatherUPBIndi(app=None)
+    """
+
+    __all__ = ['WeatherUPBIndi',
+               ]
+
+    logger = logging.getLogger(__name__)
+    log = CustomLogger(logger, {})
+
+    # update rate to 1000 milli seconds for setting indi server
+    UPDATE_RATE = 1000
+
+    def __init__(self, app=None, signals=None, data=None):
+        super().__init__(app=app, data=data, threadPool=app.threadPool)
+
+        self.signals = signals
+        self.data = data
+
+    def setUpdateConfig(self, deviceName):
+        """
+        _setUpdateRate corrects the update rate of weather devices to get an defined
+        setting regardless, what is setup in server side.
+
+        :param deviceName:
+        :return: success
+        """
+
+        if deviceName != self.deviceName:
+            return False
+
+        if self.device is None:
+            return False
+
+        # setting polling updates in driver
+
+        update = self.device.getNumber('POLLING')
+
+        if 'PERIOD' not in update:
+            return False
+
+        if update.get('PERIOD', 0) == self.UPDATE_RATE:
+            return True
+
+        update['PERIOD'] = self.UPDATE_RATE
+        suc = self.client.sendNewNumber(deviceName=deviceName,
+                                        propertyName='POLLING',
+                                        elements=update,
+                                        )
+
+        return suc

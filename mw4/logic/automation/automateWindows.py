4baa0d774ca18db80af93d1a2cf93f3c51b1ae78
mworion <michael@wuertenberger.org>
2020-10-10 13:43:04 +0200

    update logic for it

--- mw4/logic/automation/automateWindows.py
+++ mw4/logic/automation/automateWindows.py
@@ -54,30 +54,6 @@ class AutomationWindows(QObject):
         if self.TARGET_DIR == '':
             self.TARGET_DIR = os.getcwd()+'/config/'
 
-    def initConfig(self):
-        """
-        initConfig read the key out of the configuration dict and stores it to the gui
-        elements. if some initialisations have to be proceeded with the loaded persistent
-        data, they will be launched as well in this method.
-
-        :return: True for test purpose
-        """
-
-        config = self.app.config['mainW']
-
-    def storeConfig(self):
-        """
-        storeConfig writes the keys to the configuration dict and stores. if some
-        saving has to be proceeded to persistent data, they will be launched as
-        well in this method.
-
-        :return: True for test purpose
-        """
-
-        config = self.app.config['mainW']
-        config['checkFilterMPC'] = self.ui.checkFilterMPC.isChecked()
-        config['filterExpressionMPC'] = self.ui.filterExpressionMPC.text()
-
     @staticmethod
     def getRegistrationKeyPath():
         """
@@ -139,37 +115,6 @@ class AutomationWindows(QObject):
             self.logger.info('Name: {0}, Path: {1}'.format(self.appName, self.appInstallPath))
         else:
             self.logger.info('Application 10micron Updater  not found on computer')
-
-    def filterFileMPC(self, directory, filename, expression, start, end):
-        numberEntry = 0
-        with open(directory + filename, 'r') as inFile, open(directory + 'filter.mpc', 'w') as outFile:
-            for line in inFile:
-                searchExp = expression.split(',')
-                for exp in searchExp:
-                    if line.find(exp, start, end) != -1:
-                        outFile.write(line)
-                        numberEntry += 1
-        if numberEntry == 0:
-            return False
-        else:
-            self.app.messageQueue.put('Found {0} target(s) in MPC file: {1}\n'.format(numberEntry, filename))
-            self.logger.info('Found {0} target(s) in MPC file: {1}!'.format(numberEntry, filename))
-            return True
-
-    def downloadFile(self, url, filename):
-        try:
-            r = requests.get(url, stream=True)
-            if r.status_code == 200:
-                numberOfChunks = 0
-                with open(filename, 'wb') as f:
-                    for chunk in r.iter_content(128):
-                        numberOfChunks += 1
-                        f.write(chunk)
-            self.app.messageQueue.put('Downloaded {0} Bytes\n'.format(128 * numberOfChunks))
-        except Exception as e:
-            self.logger.error('Download of {0} failed, error{1}'.format(url, e))
-            self.app.messageQueue.put('#BRDownload Error {0}\n'.format(e))
-        return
 
     def uploadMount(self):
         actual_work_dir = ''

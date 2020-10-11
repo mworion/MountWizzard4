e101a46ef71b612b9454f5ca0b7a125c21922264
mworion <michael@wuertenberger.org>
2020-10-11 21:28:52 +0200

    update to 0.170.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,8 +22,8 @@ copyright = '2020, mworion'
 author = 'mworion'
 
 # The full version, including alpha/beta/rc tags
-version = '0.160.3'
-version = '0.160.3'
+version = '0.170.0'
+version = '0.170.0'
 master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------

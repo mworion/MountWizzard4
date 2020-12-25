e0416665d797376569b23c6f9abecc265b8f355d
mworion <michael@wuertenberger.org>
2020-12-25 17:25:50 +0100

    lgtm update

--- mw4/gui/mainWmixin/tabMount.py
+++ mw4/gui/mainWmixin/tabMount.py
@@ -20,7 +20,6 @@ import datetime
 # external packages
 import PyQt5.QtCore
 import PyQt5.QtWidgets
-from mountcontrol.convert import stringToAngle
 from skyfield.toposlib import Topos
 
 # local import

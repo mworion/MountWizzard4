8a51312b064c0a277547050bc5f851d33983e594
mworion <michael@wuertenberger.org>
2021-01-04 12:12:11 +0100

    update copyright date

--- mw4/base/alpacaBase.py
+++ mw4/base/alpacaBase.py
@@ -10,7 +10,7 @@
 # Python-based Tool for interaction with the 10micron mounts
 # GUI with PyQT5 for python
 #
-# written in python3, (c) 2019, 2020 by mworion
+# written in python3, (c) 2019-2021 by mworion
 #
 # Licence APL2.0
 #

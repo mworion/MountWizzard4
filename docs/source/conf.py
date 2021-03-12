2725dc16f49ced7dea969933c866c375a7999866
mworion <michael@wuertenberger.org>
2021-03-12 20:28:11 +0100

    remove ff

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,8 +39,8 @@ copyright = '2021, mworion'
 author = 'mworion'
 
 # The full version, including alpha/beta/rc tags
-version = '1.1.0b5'
-release = '1.1.0b5'
+version = '1.1.0b6'
+release = '1.1.0b6'
 master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------

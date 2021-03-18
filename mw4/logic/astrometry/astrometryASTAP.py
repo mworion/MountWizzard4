345c7ac641c212f392292c16c2116320c81f19fb
mworion <michael@wuertenberger.org>
2021-03-18 21:40:57 +0100

    adding index path to log

--- mw4/logic/astrometry/astrometryASTAP.py
+++ mw4/logic/astrometry/astrometryASTAP.py
@@ -297,8 +297,7 @@ class AstrometryASTAP(object):
         else:
             sucIndex = True
 
-        t = f'ASTAP OK, app:{program}, '
-        t = t + f'G17:{isG17}, G18:{isG18}, H17:{isH17}, H18:{isH18}'
-        self.log.info(t)
+        self.log.info(f'ASTAP OK, app:{program}, index:{self.indexPath}')
+        self.log.info(f'Index: G17:{isG17}, G18:{isG18}, H17:{isH17}, H18:{isH18}')
 
         return sucProgram, sucIndex

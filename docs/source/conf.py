ea7261fdcd23eff3a3ddf6be46fbf1b7e897c8cc
mworion <michael@wuertenberger.org>
2021-03-28 15:09:48 +0200

    adjusting readme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,8 +39,8 @@ copyright = '2021, mworion'
 author = 'mworion'
 
 # The full version, including alpha/beta/rc tags
-version = '1.1.0b12'
-release = '1.1.0b12'
+version = '1.1.0'
+release = '1.1.0'
 master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ with open('notes.txt', 'w') as f:
 
 setup(
     name='mountwizzard4',
-    version='1.1.0',
+    version='1.2.0b0',
     packages=[
         'mw4',
         'mw4.base',

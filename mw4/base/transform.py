b945c03c3ee5508be5774147246b3eb58ccbfad7
mworion <michael@wuertenberger.org>
2021-04-11 20:36:10 +0200

    adding basic information about discussions

--- mw4/base/transform.py
+++ mw4/base/transform.py
@@ -159,3 +159,48 @@ def diffModulus(x, y, m):
     diff = abs(x - y)
     diff = abs(diff % m)
     return min(diff, abs(diff - m))
+
+
+"""
+remarks from discussions about correct transformation thread in 10micron forum
+https://www.10micron.eu/forum/viewtopic.php?f=18&t=791
+
+The correct result is SOFA Atci13 (at least, it is consistent with the 10micron 
+coordinate transformation, with a < 1 arcsecond difference). It seems that 
+libNOVA/NOVAS are correct, but they don't account for nutation and light 
+aberration (the difference between these two is negligible) - i.e. these are 
+coordinates of the "mean equinox of date".
+
+The "Jnow" coordinates accepted by the mount shouldn't account for refraction 
+(well: it depends on the side you are looking from, let's say that in order to 
+compute the Jnow coordinates from the J2000.0 coordinates you don't need to 
+correct for refraction).
+
+The refraction correction is done transparently by the mount using the current 
+refraction parameters when it converts the Jnow coordinates to angular readouts 
+of the encoders (and back).
+
+If Atco13 with refraction set to zero and Atci13 agree to 1 arcsecond, I'd say 
+that it's equivalent to use any of them.
+
+Sorry, I was meaning: NOVAS is "correct" in the sense that it computes a correct 
+precession, but does not account for nutation, so it isn't "correct" in the sense 
+that is the right transformation to use. If you take the NOVAS result and apply 
+nutation/light aberration, you should obtain the right transformation.
+
+Atci13 (or Atco13 with zero refraction), which account for precession, nutation 
+and light aberration, but NOT for refraction, seems ok.
+
+By the way, there are some subtle effects of less than one arcsecond that aren't 
+worth to model (such as diurnal aberration for example), but these can be the 
+source of the small differences between different algorithms.
+
+Finally:
+I looked at the SOFA AstrometryTools documentation. The algorithms used in the 
+mount don't use the CIRS (celestial intermediate reference system), but use what 
+it is defined as "classical geocentric apparent place" in the "Quick start" 
+section of the document. So you would obtain the correct coordinates by using 
+iauAtci13. Also in section 2.2 - Current Coordinates of that document the 
+definitions are consistent. Residual (and negligible) differences will be due to 
+the fact that the mount algorithms are less refined that the ones used in SOFA.
+"""

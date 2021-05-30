f05e21017884c7558675455ff2ff3509c2ec27e4
mworion <michael@wuertenberger.org>
2021-05-30 13:13:22 +0200

    fix lgtm

--- mw4/gui/mainWmixin/tabAlmanac.py
+++ mw4/gui/mainWmixin/tabAlmanac.py
@@ -19,7 +19,7 @@
 from dateutil.tz import tzlocal
 
 # external packages
-from PyQt5.QtGui import QPixmap, QPainter, QPen, QColor, QFont
+from PyQt5.QtGui import QPixmap, QPainter, QPen, QColor
 from PyQt5.QtCore import Qt, QPointF
 from PyQt5.QtWidgets import QApplication
 from skyfield import almanac

473b63d06c7450f09ba38fe3c629881e888e3476
mworion <michael@wuertenberger.org>
2021-08-05 17:33:49 +0200

    define v34

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,8 +39,8 @@ copyright = '2021, mworion'
 author = 'mworion'
 
 # The full version, including alpha/beta/rc tags
-version = '2.0.0b33'
-release = '2.0.0b33'
+version = '2.0.0b34'
+release = '2.0.0b34'
 master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ with open('notes.txt', 'w') as f:
 
 setup(
     name='mountwizzard4',
-    version='2.0.0b33',
+    version='2.0.0b34',
     packages=[
         'mw4',
         'mw4.base',

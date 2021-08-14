ddf98ee75ed06b5bd8b24cf382e3799d6854cb8e
mworion <michael@wuertenberger.org>
2021-08-14 17:09:20 +0200

    update image

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,8 +39,8 @@ copyright = '2021, mworion'
 author = 'mworion'
 
 # The full version, including alpha/beta/rc tags
-version = '2.0.0b39'
-release = '2.0.0b39'
+version = '2.0.0b40'
+release = '2.0.0b40'
 master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ with open('notes.txt', 'w') as f:
 
 setup(
     name='mountwizzard4',
-    version='2.0.0b39',
+    version='2.0.0b40',
     packages=[
         'mw4',
         'mw4.base',

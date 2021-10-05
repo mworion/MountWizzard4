a6f2dbb69a6fdd92bc8107f3a1fd9bdd4efa9d37
mworion <michael@wuertenberger.org>
2021-10-05 16:50:29 +0200

    correct install scripts

--- support/2.0/Ubuntu/MW4_Install_aarch64_20_04.sh
+++ support/2.0/Ubuntu/MW4_Install_aarch64_20_04.sh
@@ -123,6 +123,8 @@ pip install "${WHL}"/sgp4-2.20"${PY}"manylinux2014"${ARCH}" >> install.log 2>&1
 pip install "${WHL}"/PyQt5_sip-12.8.1"${PY}"linux"${ARCH}" >> install.log 2>&1
 pip install "${WHL}"/PyQt5-5.15.4-cp36.cp37.cp38.cp39-abi3-manylinux2014"${ARCH}" >> install.log 2>&1
 
+exit
+
 echo
 echo --------------------------------------------------------
 echo installing mountwizzard4

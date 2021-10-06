1168f338622b8132ddb3fa0a64824fd17a6710e3
mworion <michael@wuertenberger.org>
2021-10-06 22:06:51 +0200

    update wheels

--- support/2.0/Ubuntu/MW4_Install_aarch64_18_04.sh
+++ support/2.0/Ubuntu/MW4_Install_aarch64_18_04.sh
@@ -90,36 +90,25 @@ echo --------------------------------------------------------
 echo start virtualenv and update tools
 echo --------------------------------------------------------
 
-source ./venv/bin/activate venv >> install.log 2>&1
+source venv/bin/activate >> install.log 2>&1
 pip install pip --upgrade >> install.log 2>&1
 pip install setuptools --upgrade >> install.log 2>&1
 pip install wheel --upgrade >> install.log 2>&1
+
+GITHUB="https://github.com/mworion/MountWizzard4/tree/master/support"
+UBUNTU="/2.0/wheels/ubuntu18.04"
 
 echo
 echo --------------------------------------------------------
 echo installing precompiled packages
 echo --------------------------------------------------------
 
-WHL="https://github.com/mworion/MountWizzard4/tree/master/support/2.0/wheels/ubuntu18.04"
-ARCH="_aarch64.whl  >> install.log 2>&1"
-PY37="-cp37-37m-"
-PY38="-cp38-cp38-"
-PY39="-cp39-cp39-"
-
-if [ "${P_VER:0:9}" == "python3.9" ]; then
-  PY="${PY39}"
-elif [ "${P_VER:0:9}" == "python3.8" ]; then
-  PY="${PY38}"
-elif [ "${P_VER:0:9}" == "python3.7" ]; then
-  PY="${PY37}"
-fi
-  
-pip install "${WHL}"/pyerfa-2.0.0"${PY}"linux"${ARCH}"
-pip install "${WHL}"/astropy-4.3.1"${PY}"linux"${ARCH}"
-pip install "${WHL}"/sep-1.2.0"${PY}"linux"${ARCH}"
-pip install "${WHL}"/sgp4-2.20"${PY}"manylinux2014"${ARCH}"
-pip install "${WHL}"/PyQt5_sip-12.9.0"${PY}"linux"${ARCH}"
-pip install "${WHL}"/PyQt5-5.15.4-cp36.cp37.cp38.cp39-abi3-manylinux2014"${ARCH}"
+pip install numpy==1.21.2 --find-links "${GITHUB}${UBUNTU}" >> install.log 2>&1
+pip install pyerfa==2.0.0 --find-links "${GITHUB}${UBUNTU}" >> install.log 2>&1
+pip install sgp4==2.20 --find-links "${GITHUB}${UBUNTU}" >> install.log 2>&1
+pip install sep==1.2.0 --find-links "${GITHUB}${UBUNTU}" >> install.log 2>&1
+pip install PyQt5-sip==12.8.1 --find-links "${GITHUB}${UBUNTU}" >> install.log 2>&1
+pip install PyQt5==5.15.4 --find-links "${GITHUB}${UBUNTU}" >> install.log 2>&1
 
 echo
 echo --------------------------------------------------------

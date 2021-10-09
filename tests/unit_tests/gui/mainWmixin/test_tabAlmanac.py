76b3e1a30d71c6932a3381cbf72727010a7e6f4e
mworion <michael@wuertenberger.org>
2021-10-09 20:56:36 +0200

    optimized polar align star selection

--- tests/unit_tests/gui/mainWmixin/test_tabAlmanac.py
+++ tests/unit_tests/gui/mainWmixin/test_tabAlmanac.py
@@ -176,10 +176,11 @@ def test_displayTwilightData_1(function):
 
 
 def test_calcMoonPhase_1(function):
-    a, b, c = function.calcMoonPhase()
+    a, b, c, d = function.calcMoonPhase()
     assert a is not None
     assert b is not None
     assert c is not None
+    assert d is not None
 
 
 def test_generateMoonMask_1(function):
@@ -201,7 +202,7 @@ def test_generateMoonMask_4(function):
 def test_updateMoonPhase_1(function):
     with mock.patch.object(function,
                            'calcMoonPhase',
-                           return_value=(20, 45, .20)):
+                           return_value=(20, 45, .20, 0)):
         suc = function.updateMoonPhase()
         assert suc
 
@@ -209,7 +210,7 @@ def test_updateMoonPhase_1(function):
 def test_updateMoonPhase_2(function):
     with mock.patch.object(function,
                            'calcMoonPhase',
-                           return_value=(45, 135, .45)):
+                           return_value=(45, 135, .45, 0)):
         suc = function.updateMoonPhase()
         assert suc
 

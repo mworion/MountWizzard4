3df07b8ccf1ced25ce4588573cf74aebb39d24d6
mworion <michael@wuertenberger.org>
2021-10-31 18:58:21 +0100

    fixing doc build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,7 +49,8 @@ master_doc = 'index'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autosectionlabel']
+extensions = ['sphinx.ext.autosectionlabel',
+              'rst2pdf.pdfbuilder']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']

13c7323abc3d75ac8a84b86c3b925c95c7652dca
mworion <michael@wuertenberger.org>
2021-11-13 12:19:07 +0100

    correcting script as prebuild wheels are linux not manylinux

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,8 +40,8 @@ copyright = '2021, mworion'
 author = 'mworion'
 
 # The full version, including alpha/beta/rc tags
-version = '2.1.1b0'
-release = '2.1.1b0'
+version = '2.1.1b1'
+release = '2.1.1b1'
 master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------

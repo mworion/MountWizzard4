7d6df667f44c9c46de8d1bf5d99dc4a80593ba54
mworion <michael@wuertenberger.org>
2021-12-04 13:13:58 +0100

    update ci

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,8 +40,8 @@ copyright = '2021, mworion'
 author = 'mworion'
 
 # The full version, including alpha/beta/rc tags
-version = '2.1.5b0'
-release = '2.1.5b0'
+version = '2.1.5'
+release = '2.1.5'
 master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------

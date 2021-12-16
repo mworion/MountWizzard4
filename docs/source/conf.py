e1df0030827f156075a0e5663b0e5c050b153028
mworion <michael@wuertenberger.org>
2021-12-16 20:36:31 +0100

    robust dome

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,8 +40,8 @@ copyright = '2021, mworion'
 author = 'mworion'
 
 # The full version, including alpha/beta/rc tags
-version = '2.1.7b0'
-release = '2.1.7b0'
+version = '2.1.7b1'
+release = '2.1.7b1'
 master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------

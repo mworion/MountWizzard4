d4c734dd73bfbc6eeac4828b704d5bb7ee1ebcda
mworion <michael@wuertenberger.org>
2022-01-23 16:05:16 +0100

    fix workflow

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,8 +40,8 @@ copyright = '2021, mworion'
 author = 'mworion'
 
 # The full version, including alpha/beta/rc tags
-version = '2.2.0b9'
-release = '2.2.0b9'
+version = '2.2.0b10'
+release = '2.2.0b10'
 master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------

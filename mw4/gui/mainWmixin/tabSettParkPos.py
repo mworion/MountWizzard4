96d0e62594b369011d7dc2ff26188851473699fc
Michael <michael.wuertenberger@t-online.de>
2022-01-31 17:51:40 +0100

    refactor for offering single digit precision

--- mw4/gui/mainWmixin/tabSettParkPos.py
+++ mw4/gui/mainWmixin/tabSettParkPos.py
@@ -37,7 +37,6 @@ class SettParkPos(object):
         self.posAlt = list()
         self.posAz = list()
         self.posSaveButtons = list()
-
         self.setupParkPosGui()
         for posText in self.posTexts:
             posText.editingFinished.connect(self.updateParkPosButtonText)
@@ -54,21 +53,18 @@ class SettParkPos(object):
 
         :return: True for test purpose
         """
-
         config = self.app.config['mainW']
-
         for i, textField in enumerate(self.posTexts):
             keyConfig = 'posText{0:1d}'.format(i)
             textField.setText(config.get(keyConfig, 'Park Pos {0:1d}'.format(i)))
         for i, textField in enumerate(self.posAlt):
             keyConfig = 'posAlt{0:1d}'.format(i)
-            textField.setText(config.get(keyConfig, '-'))
+            textField.setText(config.get(keyConfig, ''))
         for i, textField in enumerate(self.posAz):
             keyConfig = 'posAz{0:1d}'.format(i)
-            textField.setText(config.get(keyConfig, '-'))
+            textField.setText(config.get(keyConfig, ''))
         self.updateParkPosButtonText()
         self.ui.parkMountAfterSlew.setChecked(config.get('parkMountAfterSlew', False))
-
         return True
 
     def storeConfig(self):
@@ -79,9 +75,7 @@ class SettParkPos(object):
 
         :return: True for test purpose
         """
-
         config = self.app.config['mainW']
-
         for i, textField in enumerate(self.posTexts):
             keyConfig = 'posText{0:1d}'.format(i)
             config[keyConfig] = textField.text()
@@ -92,7 +86,6 @@ class SettParkPos(object):
             keyConfig = 'posAz{0:1d}'.format(i)
             config[keyConfig] = textField.text()
         config['parkMountAfterSlew'] = self.ui.parkMountAfterSlew.isChecked()
-
         return True
 
     def setupParkPosGui(self):
@@ -109,7 +102,6 @@ class SettParkPos(object):
             self.posAlt.append(eval('self.ui.posAlt{0:1d}'.format(i)))
             self.posAz.append(eval('self.ui.posAz{0:1d}'.format(i)))
             self.posSaveButtons.append(eval('self.ui.posSave{0:1d}'.format(i)))
-
         return True
 
     def updateParkPosButtonText(self):
@@ -119,10 +111,8 @@ class SettParkPos(object):
 
         :return: true for test purpose
         """
-
         for button, textField in zip(self.posButtons, self.posTexts):
             button.setText(textField.text())
-
         return True
 
     def parkAtPos(self):
@@ -131,10 +121,8 @@ class SettParkPos(object):
         """
         self.app.mount.signals.slewFinished.disconnect(self.parkAtPos)
         suc = self.app.mount.obsSite.parkOnActualPosition()
-
         if not suc:
             self.app.message.emit('Cannot park at current position', 2)
-
         return suc
 
     def slewToParkPos(self):
@@ -145,7 +133,6 @@ class SettParkPos(object):
 
         :return: success
         """
-
         for button, posText, alt, az in zip(self.posButtons,
                                             self.posTexts,
                                             self.posAlt,
@@ -158,14 +145,13 @@ class SettParkPos(object):
             try:
                 altValue = float(alt.text())
                 azValue = float(az.text())
-
             except Exception as e:
-                self.log.critical(f'no usable values in data: error {e}')
-                self.app.message.emit('Missing correct entries in settings', 2)
+                self.log.error(f'No usable values in data: error [{e}]')
+                t = 'Missing correct entries in Alt/Az park pos settings'
+                self.app.message.emit(t, 2)
                 return False
 
             posTextValue = posText.text()
-
             if altValue < -5:
                 altValue = -5
             elif altValue > 90:
@@ -174,21 +160,17 @@ class SettParkPos(object):
 
             suc = self.app.mount.obsSite.setTargetAltAz(alt_degrees=altValue,
                                                         az_degrees=azValue)
-
             if suc:
                 suc = self.app.mount.obsSite.startSlewing(slewType='notrack')
-
             if not suc:
                 self.app.message.emit(f'Cannot slew to [{posTextValue}]', 2)
                 return False
 
             self.app.message.emit(f'Slew to [{posTextValue}]', 0)
-
             if not self.ui.parkMountAfterSlew.isChecked():
                 return True
 
             self.app.mount.signals.slewFinished.connect(self.parkAtPos)
-
             return True
 
         return False
@@ -200,9 +182,7 @@ class SettParkPos(object):
 
         :return: success
         """
-
         obs = self.app.mount.obsSite
-
         if not obs.Alt:
             return False
         if not obs.Az:
@@ -210,13 +190,10 @@ class SettParkPos(object):
 
         for button, alt, az in zip(self.posSaveButtons,
                                    self.posAlt,
-                                   self.posAz,
-                                   ):
-
+                                   self.posAz):
             if button != self.sender():
                 continue
 
-            alt.setText(f'{obs.Alt.degrees:3.0f}')
-            az.setText(f'{obs.Az.degrees:3.0f}')
-
+            alt.setText(f'{obs.Alt.degrees:4.1f}')
+            az.setText(f'{obs.Az.degrees:4.1f}')
         return True

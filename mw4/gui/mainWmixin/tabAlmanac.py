08c7e0ef97773532bca9a39d1dddc87d3f14376d
mworion <michael@wuertenberger.org>
2022-02-04 09:20:02 +0100

    remove unnecessary import

--- mw4/gui/mainWmixin/tabAlmanac.py
+++ mw4/gui/mainWmixin/tabAlmanac.py
@@ -20,7 +20,7 @@ from dateutil.tz import tzlocal
 
 # external packages
 from PyQt5.QtGui import QPixmap, QPainter, QPen, QColor
-from PyQt5.QtCore import Qt, QPointF, QRect
+from PyQt5.QtCore import Qt, QPointF
 from PyQt5.QtWidgets import QApplication
 from skyfield import almanac
 from skyfield.trigonometry import position_angle_of

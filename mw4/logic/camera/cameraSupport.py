b78736098a703ec3efd52ec4019dcf50f70c50cd
Michael <michael@wuertenberger.org>
2022-02-14 10:36:39 +0100

    remove unnecessary import

--- mw4/logic/camera/cameraSupport.py
+++ mw4/logic/camera/cameraSupport.py
@@ -24,7 +24,6 @@ import numpy as np
 
 # local imports
 from gui.utilities.toolsQtWidget import sleepAndEvents
-from base.transform import JNowToJ2000
 from mountcontrol.convert import formatDstrToText
 
 

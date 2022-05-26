f8df57e4e7b49d7682957f2db20e6485d3d27f91
mworion <michael@wuertenberger.org>
2022-05-26 14:06:14 +0200

    check alert message in indi camera

--- tests/unit_tests/gui/mainWmixin3/test_tabSettMisc.py
+++ tests/unit_tests/gui/mainWmixin3/test_tabSettMisc.py
@@ -30,11 +30,10 @@ import importlib_metadata
 # local import
 from gui.mainWmixin.tabSettMisc import SettMisc
 import gui.mainWmixin.tabSettMisc
-from tests.unit_tests.unitTestAddOns.baseTesApp import App
+from tests.unit_tests.unitTestAddOns.baseTestApp import App
 from gui.widgets.main_ui import Ui_MainWindow
 from gui.utilities.toolsQtWidget import MWidget
 from base.loggerMW import setupLogging
-import base.packageConfig as pConf
 setupLogging()
 
 

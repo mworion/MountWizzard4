44108a2c6031f973277f13dcb75e2e8a2a432768
mworion <michael@wuertenberger.org>
2022-05-26 19:40:22 +0200

    refactor tests

--- tests/unit_tests/gui/extWindows/simulator/test_s_horizon.py
+++ tests/unit_tests/gui/extWindows/simulator/test_s_horizon.py
@@ -20,71 +20,54 @@ import pytest
 
 # external packages
 from PyQt5.Qt3DCore import QEntity
-from PyQt5.QtCore import QObject
-from mountcontrol.mount import Mount
-from skyfield.api import wgs84
 
 # local import
+from tests.unit_tests.unitTestAddOns.baseTestApp import App
 from gui.extWindows.simulator.horizon import SimulatorHorizon
 
 
 @pytest.fixture(autouse=True, scope='function')
-def module_setup_teardown():
-    global app
-
-    class Test1:
-        horizonP = [(45, 45), (50, 50)]
-
-    class Test(QObject):
-        data = Test1()
-        mount = Mount(host='localhost', MAC='00:00:00:00:00:00', verbose=False,
-                      pathToData='tests/workDir/data')
-        mount.obsSite.location = wgs84.latlon(latitude_degrees=20,
-                                              longitude_degrees=10,
-                                              elevation_m=500)
-        mwGlob = {'modelDir': 'tests/workDir/model',
-                  'imageDir': 'tests/workDir/image'}
-        uiWindows = {'showImageW': {'classObj': None}}
-
-    app = SimulatorHorizon(app=Test())
-    yield
+def function():
+    func = SimulatorHorizon(app=App())
+    yield func
 
 
-def test_createWall_1():
-    val = app.createWall(QEntity(), 0, 0, 10)
+def test_createWall_1(function):
+    val = function.createWall(QEntity(), 0, 0, 10)
     assert isinstance(val, QEntity)
 
 
-def test_create_1(qtbot):
+def test_create_1(function):
     e = QEntity()
-    suc = app.create(e, False)
+    suc = function.create(e, False)
     assert not suc
 
 
-def test_create_2(qtbot):
+def test_create_2(function):
     e = QEntity()
-    app.modelRoot = e
-    app.model = {'test': {'e': e}}
-    suc = app.create(e, False)
+    function.modelRoot = e
+    function.model = {'test': {'e': e}}
+    suc = function.create(e, False)
     assert not suc
 
 
-def test_create_3(qtbot):
+def test_create_3(function):
     e = QEntity()
-    app.app.data.horizonP = None
-    app.modelRoot = e
-    app.model = {'test': {'e': e}}
-    suc = app.create(e, True)
+    function.app.data.horizonP = None
+    function.modelRoot = e
+    function.model = {'test': {'e': e}}
+    suc = function.create(e, True)
     assert not suc
 
 
-def test_create_4(qtbot):
-    app.horizon = [
+def test_create_4(function):
+    function.horizon = [
         {'e': QEntity()},
     ]
-    app.horizonRoot = QEntity()
+    function.app.data.horizonP = [(0, 0), (10, 10)]
+    function.horizonRoot = QEntity()
     e = QEntity()
-    app.modelRoot = e
-    app.model = {'test': {'e': e}}
-    suc = app.create(e, True)
+    function.modelRoot = e
+    function.model = {'test': {'e': e}}
+    suc = function.create(e, True)
     assert suc

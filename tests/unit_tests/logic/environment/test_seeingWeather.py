d2502624189f0783c3851093bb54b4c60c966142
mworion <michael@wuertenberger.org>
2022-06-14 08:56:11 +0200

    add tests seeing data

---
+++ tests/unit_tests/logic/environment/test_seeingWeather.py
@@ -0,0 +1,222 @@
+############################################################
+# -*- coding: utf-8 -*-
+#
+#       #   #  #   #   #    #
+#      ##  ##  #  ##  #    #
+#     # # # #  # # # #    #  #
+#    #  ##  #  ##  ##    ######
+#   #   #   #  #   #       #
+#
+# Python-based Tool for interaction with the 10micron mounts
+# GUI with PyQT5 for python
+#
+# written in python3, (c) 2019-2022 by mworion
+# Licence APL2.0
+#
+###########################################################
+# standard libraries
+import pytest
+import unittest.mock as mock
+import os
+import json
+
+# external packages
+import requests
+
+# local import
+from tests.unit_tests.unitTestAddOns.baseTestApp import App
+from logic.environment.seeingWeather import SeeingWeather
+from base.loggerMW import setupLogging
+setupLogging()
+
+
+@pytest.fixture(autouse=True, scope='function')
+def function():
+    class Test1:
+        status_code = 200
+
+        @staticmethod
+        def json():
+            return 'test'
+
+    with mock.patch.object(requests,
+                           'get',
+                           return_value=Test1()):
+        func = SeeingWeather(app=App())
+        yield func
+
+
+def test_properties(function):
+    with mock.patch.object(function,
+                           'pollSeeingData'):
+        function.keyAPI = 'test'
+        assert function.keyAPI == 'test'
+        function.online = True
+        assert function.online
+
+
+def test_startCommunication_(function):
+    function.running = False
+    with mock.patch.object(function,
+                           'pollSeeingData'):
+        suc = function.startCommunication()
+        assert suc
+        assert function.running
+
+
+def test_stopCommunication_1(function):
+    function.running = True
+    suc = function.stopCommunication()
+    assert suc
+    assert not function.running
+
+
+def test_processSeeingData_1(function):
+    with mock.patch.object(os.path,
+                           'isfile',
+                           return_value=False):
+        suc = function.processSeeingData()
+        assert not suc
+
+
+def test_processSeeingData_2(function):
+    with mock.patch.object(json,
+                           'load',
+                           return_value={}):
+        suc = function.processSeeingData()
+        assert suc
+
+
+def test_workerGetSeeingData_1(function):
+    class Test:
+        status_code = 300
+    with mock.patch.object(requests,
+                           'get',
+                           return_value=Test()):
+        suc = function.workerGetSeeingData('http://localhost')
+        assert not suc
+
+
+def test_workerGetSeeingData_3(function):
+    class Test:
+        status_code = 300
+    with mock.patch.object(requests,
+                           'get',
+                           side_effect=Exception(),
+                           return_value=Test()):
+        suc = function.workerGetSeeingData('http://localhost')
+        assert not suc
+
+
+def test_workerGetSeeingData_4(function):
+    class Test:
+        status_code = 300
+    with mock.patch.object(requests,
+                           'get',
+                           side_effect=TimeoutError(),
+                           return_value=Test()):
+        suc = function.workerGetSeeingData('http://localhost')
+        assert not suc
+
+
+def test_workerGetSeeingData_5(function):
+    class Test:
+        status_code = 200
+
+        @staticmethod
+        def json():
+            return 'test'
+
+    with mock.patch.object(requests,
+                           'get',
+                           return_value=Test()):
+        suc = function.workerGetSeeingData('http://localhost')
+        assert suc
+
+
+def test_getSeeingData(function):
+    with mock.patch.object(function.threadPool,
+                           'start'):
+        suc = function.getSeeingData('test')
+        assert suc
+
+
+def test_loadingFileNeeded_1(function):
+    with mock.patch.object(os.path,
+                           'isfile',
+                           return_value=False):
+        suc = function.loadingFileNeeded('test', 1)
+        assert suc
+
+
+def test_loadingFileNeeded_2(function):
+    with mock.patch.object(os.path,
+                           'isfile',
+                           return_value=True):
+        with mock.patch.object(function.app.mount.obsSite.loader,
+                               'days_old',
+                               return_value=1):
+            suc = function.loadingFileNeeded('test', 1)
+            assert suc
+
+
+def test_loadingFileNeeded_3(function):
+    with mock.patch.object(os.path,
+                           'isfile',
+                           return_value=True):
+        with mock.patch.object(function.app.mount.obsSite.loader,
+                               'days_old',
+                               return_value=1):
+            suc = function.loadingFileNeeded('test', 25)
+            assert not suc
+
+
+def test_pollSeeingData_1(function):
+    function.online = False
+    function.running = True
+    function.apiKey = ''
+    with mock.patch.object(function,
+                           'stopCommunication'):
+        suc = function.pollSeeingData()
+        assert not suc
+
+
+def test_pollSeeingData_2(function):
+    function.online = True
+    function.running = False
+    function.apiKey = ''
+    suc = function.pollSeeingData()
+    assert not suc
+
+
+def test_pollSeeingData_3(function):
+    function.online = True
+    function.running = True
+    function.apiKey = ''
+    suc = function.pollSeeingData()
+    assert not suc
+
+
+def test_pollSeeingData_4(function):
+    function.online = True
+    function.running = True
+    function.apiKey = 'test'
+    with mock.patch.object(function,
+                           'loadingFileNeeded',
+                           return_value=False):
+        suc = function.pollSeeingData()
+        assert suc
+
+
+def test_pollSeeingData_5(function):
+    function.online = True
+    function.running = True
+    function.apiKey = 'test'
+    with mock.patch.object(function,
+                           'loadingFileNeeded',
+                           return_value=True):
+        with mock.patch.object(function,
+                               'getSeeingData'):
+            suc = function.pollSeeingData()
+            assert suc
+

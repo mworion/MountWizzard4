f507c72b6b813ff17b34ad1ff2ee3391a321198f
mworion <michael@wuertenberger.org>
2022-06-17 11:22:02 +0200

    cancel run on internal error

--- mw4/gui/mainWmixin/tabModel.py
+++ mw4/gui/mainWmixin/tabModel.py
@@ -197,7 +197,7 @@ class Model:
             t += f'Images left: [{self.imageQueue.qsize()}] '
             t += f'Solves left: [{self.solveQueue.qsize()}] '
             self.log.error(t)
-            self.app.message.emit(2, 'Cancel build because of error')
+            self.app.mes.emit(2, 'Cancel build because of error')
             self.cancelBuild()
             return False
 

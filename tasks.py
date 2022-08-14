cb65e71b6de420639e86433abd52932b2c6bc19d
mworion <michael@wuertenberger.org>
2022-08-14 09:53:50 +0200

    cleanup

--- tasks.py
+++ tasks.py
@@ -116,7 +116,7 @@ def clean_mw(c):
 @task
 def image_res(c):
     printMW('changing image resolution for docs to 150 dpi')
-    files = glob.glob('./docs/source/**/*.png', recursive=True)
+    files = glob.glob('./doc/source/**/*.png', recursive=True)
     for file in files:
         print(file)
         im = Image.open(file)

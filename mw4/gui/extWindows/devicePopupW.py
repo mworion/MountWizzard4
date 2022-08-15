688f58ce1933ff8f4f06c1e0bfb2dd940dec89b8
mworion <orion.49m@googlemail.com>
2022-08-15 12:13:45 +0200

    fix for alpaca devices

--- mw4/gui/extWindows/devicePopupW.py
+++ mw4/gui/extWindows/devicePopupW.py
@@ -78,6 +78,7 @@ class DevicePopup(toolsQtWidget.MWidget):
                 'port': self.ui.alpacaPort,
                 'user': self.ui.alpacaUser,
                 'password': self.ui.alpacaPassword,
+                'deviceList': self.ui.alpacaDeviceList,
                 'updateRate': self.ui.alpacaUpdateRate,
             },
             'ascom': {

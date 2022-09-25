ce3a0ff393b033aad53912c41463abb76823bd0b
mworion <michael@wuertenberger.org>
2022-09-25 16:57:28 +0200

    declare 3.0.0b0

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -40,8 +40,8 @@ copyright = '2022, mworion'
 author = 'Michael Würtenberger'
 
 # The full version, including alpha/beta/rc tags
-version = '3.0.0a17'
-release = '3.0.0a17'
+version = '3.0.0b0'
+release = '3.0.0b0'
 master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------

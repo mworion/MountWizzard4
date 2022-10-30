74246f6ae62432a36f1419c163dd95e0c6b9a6f4
mworion <michael@wuertenberger.org>
2022-10-30 16:52:03 +0100

    update to 2.2.8a1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,8 +40,8 @@ copyright = '2021, mworion'
 author = 'mworion'
 
 # The full version, including alpha/beta/rc tags
-version = '2.2.8a0'
-release = '2.2.8a0'
+version = '2.2.8a1'
+release = '2.2.8a1'
 master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------

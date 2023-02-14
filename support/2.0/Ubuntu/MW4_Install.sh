b93974b11b31d3bd735c9b3efa26bcb4acba2ff4
d33psky <d33psky@users.noreply.github.com>
2023-02-14 19:56:13 +0100

    fix python unknown option -p

--- support/2.0/Ubuntu/MW4_Install.sh
+++ support/2.0/Ubuntu/MW4_Install.sh
@@ -95,7 +95,7 @@ echo installing mountwizzard4 - takes some time
 echo --------------------------------------------------------
 
 echo checking system packages, should be no mw4 in >> install.log  2>&1
-python -p pip list >> install.log 2>&1
+python -m pip list >> install.log 2>&1
 
 source ./venv/bin/activate venv >> install.log 2>&1
 python -m pip install pip --upgrade >> install.log 2>&1
@@ -104,7 +104,7 @@ python -m pip install wheel --upgrade >> install.log 2>&1
 python -m pip install mountwizzard4 --upgrade --no-cache-dir >> install.log 2>&1
 
 echo checking venv packages, mw4 should be present >> install.log  2>&1
-python -p pip list >> install.log 2>&1
+python -m pip list >> install.log 2>&1
 
 echo
 echo --------------------------------------------------------

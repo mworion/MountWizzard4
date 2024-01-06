cf264e91a3dcd795c674a62d25417ee8040e3c84
mworion <michael@wuertenberger.org>
2024-01-06 13:55:02 +0100

    bring release notes back in ci process

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -40,8 +40,8 @@ copyright = '2023, mworion'
 author = 'Michael Würtenberger'
 
 # The full version, including alpha/beta/rc tags
-version = '3.3.0a0'
-release = '3.3.0a0'
+version = '3.2.6b12'
+release = '3.2.6b12'
 master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ with open('notes.txt', 'w') as f:
 
 setup(
     name='mountwizzard4',
-    version='3.2.6b11',
+    version='3.2.6b12',
     packages=[
         'mw4',
         'mw4.base',

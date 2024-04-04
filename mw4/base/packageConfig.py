5934f75ccf1c91e5cb9f806ba0d4dba6b5b24e11
mworion <michael@wuertenberger.org>
2024-04-04 17:50:43 +0200

    adjust artifacts download

--- mw4/base/packageConfig.py
+++ mw4/base/packageConfig.py
@@ -24,10 +24,11 @@ isReference = False
 isWindows = platform.system() == 'Windows'
 isLinux = platform.system() == 'Linux'
 isMac = platform.system() == 'Darwin'
+enableAutomation = True
 
 
 def checkAutomation():
     verResult = Version('3.8.2') <= Version(platform.python_version())
     winResult = platform.system() == 'Windows'
     result = verResult and winResult
-    return result
+    return result and enableAutomation

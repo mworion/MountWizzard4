a1a79a5237b3b42c9f7041050774d475ae3e642e
mworion <michael@wuertenberger.org>
2024-04-25 18:36:18 +0200

    declare 3.2.6b19

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.2.6b18',
+    VERSION: '3.2.6b19',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

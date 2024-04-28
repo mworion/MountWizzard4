eddddb8793e032139d8846c09450047e6e08c991
mworion <michael@wuertenberger.org>
2024-04-28 13:02:44 +0200

    update scripts

--- generators/startup/startup.py
+++ generators/startup/startup.py
@@ -335,15 +335,15 @@ def downloadAndInstallWheels(venvContext, version=None):
         '3.1.0': {
             '3.8': [
                 'PyQt5_sip-12.11.1-cp38-cp38-linux_aarch64.whl',
-                'PyQt5-5.15.9-cp38.cp39.cp310-abi3-manylinux_2_17_aarch64.whl',
+                'PyQt5-5.15.9-cp37-abi3-manylinux_2_17_aarch64.whl',
             ],
             '3.9': [
                 'PyQt5_sip-12.11.1-cp39-cp39-linux_aarch64.whl',
-                'PyQt5-5.15.9-cp38.cp39.cp310-abi3-manylinux_2_17_aarch64.whl',
+                'PyQt5-5.15.9-cp37-abi3-manylinux_2_17_aarch64.whl',
             ],
             '3.10': [
                 'PyQt5_sip-12.11.1-cp310-cp310-linux_aarch64.whl',
-                'PyQt5-5.15.9-cp38.cp39.cp310-abi3-manylinux_2_17_aarch64.whl',
+                'PyQt5-5.15.9-cp37-abi3-manylinux_2_17_aarch64.whl',
             ],
         },
     }

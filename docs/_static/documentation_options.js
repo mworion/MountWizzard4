410b8622d2e65a3c2f0bfbc5d58c1c1e000646da
mworion <michael@wuertenberger.org>
2024-05-11 15:32:32 +0200

    update workflows

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.2.6b19',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '3.2.6b20',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

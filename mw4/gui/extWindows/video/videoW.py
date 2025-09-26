73f3f6f21b0075f44006a69d88b41d1325ab89b3
mworion <michael.wuertenberger@t-online.de>
2025-09-26 16:00:25 +0200

    cleanup multiple video windows

--- mw4/gui/extWindows/video/videoW.py
+++ mw4/gui/extWindows/video/videoW.py
@@ -17,180 +17,48 @@
 # standard libraries
 
 # external packages
-from PySide6.QtCore import Signal
-from PySide6.QtGui import QPixmap
-from PySide6.QtWidgets import QSizePolicy, QInputDialog, QLineEdit
-import cv2
-import qimage2ndarray
 
 # local import
-from gui.utilities.toolsQtWidget import MWidget
-from gui.widgets import video_ui
-from base.tpool import Worker
-from gui.utilities.toolsQtWidget import sleepAndEvents, changeStyleDynamic
+from gui.extWindows.video.videoBase import VideoWindowBase
 
 
-class VideoWindow(MWidget):
+class VideoWindow(VideoWindowBase):
     """ """
 
-    pixmapReady = Signal(object)
-
     def __init__(self, app):
-        super().__init__()
-        self.app = app
-        self.msg = app.msg
-        self.threadPool = app.threadPool
-        self.ui = video_ui.Ui_VideoDialog()
-        self.ui.setupUi(self)
-        self.running = False
-        self.capture = None
-        self.user = ""
-        self.password = ""
-        self.runningCounter = 0
-        self.worker: Worker = None
+        super().__init__(app=app)
+        self.setWindowTitle("Video Stream 1")
+        self.setObjectName("Video1")
+
+    def initConfig(self) -> None:
+        """ """
+        config = self.app.config.get("videoW1", {})
+
+        self.positionWindow(config)
+        self.ui.videoURL.setText(config.get("videoURL", ""))
+        self.ui.videoSource.setCurrentIndex(config.get("videoSource", 0))
+        self.ui.frameRate.setCurrentIndex(config.get("frameRate", 2))
+        self.user = config.get("user", "")
+        self.password = config.get("password", "")
+
+    def storeConfig(self) -> None:
+        """ """
+        configMain = self.app.config
+        configMain["videoW1"] = {}
+        config = configMain["videoW1"]
+
+        config["winPosX"] = max(self.pos().x(), 0)
+        config["winPosY"] = max(self.pos().y(), 0)
+        config["height"] = self.height()
+        config["width"] = self.width()
+        config["videoURL"] = self.ui.videoURL.text()
+        config["videoSource"] = self.ui.videoSource.currentIndex()
+        config["frameRate"] = self.ui.frameRate.currentIndex()
+        config["user"] = self.user
+        config["password"] = self.password
 
     def closeEvent(self, closeEvent) -> None:
         """ """
-        self.stopVideo()
+        self.pixmapReady.disconnect(self.receivedImage)
+        self.storeConfig()
         super().closeEvent(closeEvent)
-
-    def colorChange(self) -> None:
-        """ """
-        self.setStyleSheet(self.mw4Style)
-
-    def showWindow(self) -> None:
-        """ """
-        self.pixmapReady.connect(self.receivedImage)
-        self.ui.videoStart.clicked.connect(self.startVideo)
-        self.ui.videoStop.clicked.connect(self.stopVideo)
-        self.ui.videoSource.currentIndexChanged.connect(self.stopVideo)
-        self.ui.frameRate.currentIndexChanged.connect(self.restartVideo)
-        self.ui.authPopup.clicked.connect(self.authPopup)
-        self.app.colorChange.connect(self.colorChange)
-        self.app.update0_1s.connect(self.count)
-        changeStyleDynamic(self.ui.videoStop, "running", True)
-        self.checkAuth()
-        self.show()
-
-    def sendImage(self) -> None:
-        """ """
-        try:
-            _, frame = self.capture.retrieve()
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        except cv2.error as e:
-            self.msg.emit(2, "Video", "Compatibility", e)
-            return
-
-        image = qimage2ndarray.array2qimage(frame)
-        if not self.running:
-            return
-        self.pixmapReady.emit(QPixmap.fromImage(image))
-
-    def count(self) -> None:
-        """ """
-        self.runningCounter += 1
-
-    def workerVideo(self, source: str, frameRate: int) -> None:
-        """ """
-        self.capture = cv2.VideoCapture()
-        self.capture.setExceptionMode(True)
-        try:
-            self.capture.open(source)
-            if not self.capture.isOpened():
-                self.msg.emit(2, "Video", "Camera", f"[{source}] not available")
-                self.running = False
-                return
-        except cv2.error as e:
-            self.msg.emit(2, "Video", "Camera error", f"MSG: {e.err}")
-            self.running = False
-            return
-        except Exception as e:
-            self.msg.emit(2, "Video", "Camera error", f"MSG: {e}")
-            self.running = False
-            return
-
-        self.runningCounter = 0
-        while self.running:
-            suc = self.capture.grab()
-            if not suc:
-                break
-            if self.runningCounter % frameRate == 0:
-                self.sendImage()
-
-        self.capture.release()
-
-    def startVideo(self) -> None:
-        """ """
-        if self.user and self.password:
-            auth = f"{self.user}:{self.password}@"
-        else:
-            auth = ""
-        url = f"{auth}{self.ui.videoURL.text()}"
-        sources = ["rtsp://" + url, "http://" + url, "https://" + url, url, 0, 1, 2, 3]
-        frameCounter = [2, 5, 10, 20, 50]
-
-        sourceIndex = self.ui.videoSource.currentIndex()
-        frameRateIndex = self.ui.frameRate.currentIndex()
-        frameRate = frameCounter[frameRateIndex]
-        if not self.ui.videoURL.text() and sourceIndex == 0:
-            return
-
-        source = sources[sourceIndex]
-        self.log.info(f"Video started: source [{source}]")
-        changeStyleDynamic(self.ui.videoStart, "running", True)
-        changeStyleDynamic(self.ui.videoStop, "running", False)
-        self.running = True
-        self.worker = Worker(self.workerVideo, source, frameRate)
-        self.threadPool.start(self.worker)
-
-    def stopVideo(self) -> None:
-        """ """
-        changeStyleDynamic(self.ui.videoStart, "running", False)
-        changeStyleDynamic(self.ui.videoStop, "running", True)
-        self.pixmapReady.emit(None)
-        self.running = False
-
-    def restartVideo(self) -> None:
-        """ """
-        self.stopVideo()
-        sleepAndEvents(1000)
-        self.startVideo()
-
-    def receivedImage(self, pixmap: QPixmap) -> None:
-        """ """
-        if not self.running or pixmap is None:
-            self.ui.video.clear()
-            return
-
-        pixmap = pixmap.scaled(self.ui.video.width(), self.ui.video.height())
-        self.ui.video.setSizePolicy(QSizePolicy.Policy.Ignored, QSizePolicy.Policy.Ignored)
-        self.ui.video.setPixmap(pixmap)
-
-    def checkAuth(self) -> None:
-        """ """
-        hasAuth = self.user != "" and self.password != ""
-        changeStyleDynamic(self.ui.authPopup, "running", hasAuth)
-
-    def authPopup(self) -> None:
-        """ """
-        dlg = QInputDialog()
-        value1, ok1 = dlg.getText(
-            self,
-            "Get authentication",
-            "Username: ",
-            QLineEdit.EchoMode.Normal,
-            self.user,
-        )
-        value2, ok2 = dlg.getText(
-            self,
-            "Get authentication",
-            "Password: ",
-            QLineEdit.EchoMode.Normal,
-            self.password,
-        )
-        if not ok1 or not ok2:
-            return
-        self.user = value1
-        self.password = value2
-        self.checkAuth()
-        self.restartVideo()
--- tests/unit_tests/gui/extWindows/video/test_videoW.py
+++ tests/unit_tests/gui/extWindows/video/test_videoW.py
@@ -19,16 +19,12 @@ import pytest
 import unittest.mock as mock
 
 # external packages
-from PySide6.QtGui import QCloseEvent, QPixmap
-from PySide6.QtWidgets import QInputDialog
-import numpy as np
-import cv2
+from PySide6.QtGui import QCloseEvent
 
 # local import
 from tests.unit_tests.unitTestAddOns.baseTestApp import App
 from gui.utilities.toolsQtWidget import MWidget
 from gui.extWindows.video.videoW import VideoWindow
-import gui.extWindows.videoW
 
 
 @pytest.fixture(autouse=True, scope="module")
@@ -39,269 +35,25 @@ def function(qapp):
         func.app.threadPool.waitForDone(10000)
 
 
+def test_initConfig_1(function):
+    function.initConfig()
+
+
+def test_storeConfig_1(function):
+    if "videoW1" in function.app.config:
+        del function.app.config["videoW1"]
+
+    function.storeConfig()
+
+
+def test_storeConfig_2(function):
+    function.app.config["videoW1"] = {}
+
+    function.storeConfig()
+
+
 def test_closeEvent_1(function):
     with mock.patch.object(function, "stopVideo"):
         with mock.patch.object(MWidget, "closeEvent"):
             function.showWindow()
             function.closeEvent(QCloseEvent)
-
-
-def test_colorChange(function):
-    function.colorChange()
-
-
-def test_showWindow_1(function):
-    with mock.patch.object(function, "show"):
-        function.showWindow()
-
-
-def test_sendImage_1(function):
-    class Test:
-        @staticmethod
-        def retrieve():
-            return (True, np.ones((10, 10, 1)))
-
-    function.capture = Test()
-    function.running = False
-    with mock.patch.object(cv2, "cvtColor", return_value=np.ones((10, 10, 1))):
-        function.sendImage()
-
-
-def test_sendImage_2(function):
-    class Test:
-        @staticmethod
-        def retrieve():
-            return (True, np.ones((10, 10, 1)))
-
-    function.capture = Test()
-    function.running = False
-    with mock.patch.object(
-        cv2, "cvtColor", return_value=np.ones((10, 10, 1)), side_effect=cv2.error
-    ):
-        function.sendImage()
-
-
-def test_sendImage_3(function):
-    class Test:
-        @staticmethod
-        def retrieve():
-            return (True, np.ones((10, 10, 1)))
-
-    function.capture = Test()
-    function.running = True
-    with mock.patch.object(cv2, "cvtColor", return_value=np.ones((10, 10, 1))):
-        function.sendImage()
-
-
-def test_count(function):
-    function.runningCounter = 0
-    function.count()
-    assert function.runningCounter == 1
-
-
-def test_workerVideoStream_0(function):
-    class Test:
-        @staticmethod
-        def isOpened():
-            return True
-
-        @staticmethod
-        def grab():
-            return False
-
-        @staticmethod
-        def release():
-            return
-
-        @staticmethod
-        def open(a):
-            return
-
-        @staticmethod
-        def setExceptionMode(a):
-            return
-
-    function.running = True
-    with mock.patch.object(cv2, "VideoCapture", return_value=Test()):
-        with mock.patch.object(Test, "open", side_effect=cv2.error):
-            function.workerVideo("test", 1)
-            assert not function.running
-
-
-def test_workerVideoStream_1(function):
-    class Test:
-        @staticmethod
-        def isOpened():
-            return True
-
-        @staticmethod
-        def open(a):
-            return
-
-        @staticmethod
-        def grab():
-            return False
-
-        @staticmethod
-        def release():
-            return
-
-        @staticmethod
-        def setExceptionMode(a):
-            return
-
-    function.running = True
-    with mock.patch.object(cv2, "VideoCapture", return_value=Test()):
-        with mock.patch.object(Test, "open", side_effect=Exception):
-            function.workerVideo("test", 1)
-            assert not function.running
-
-
-def test_workerVideoStream_2(function):
-    class Test:
-        @staticmethod
-        def isOpened():
-            return False
-
-        @staticmethod
-        def open(a):
-            return
-
-        @staticmethod
-        def grab():
-            return False
-
-        @staticmethod
-        def release():
-            return
-
-        @staticmethod
-        def setExceptionMode(a):
-            return
-
-    function.running = True
-    with mock.patch.object(cv2, "VideoCapture", return_value=Test()):
-        function.workerVideo("test", 1)
-        assert not function.running
-
-
-def test_workerVideoStream_3(function):
-    class Test:
-        @staticmethod
-        def isOpened():
-            return True
-
-        @staticmethod
-        def open(a):
-            return
-
-        @staticmethod
-        def grab():
-            return False
-
-        @staticmethod
-        def release():
-            return
-
-        @staticmethod
-        def setExceptionMode(a):
-            return
-
-    function.running = True
-    with mock.patch.object(cv2, "VideoCapture", return_value=Test()):
-        with mock.patch.object(function, "sendImage"):
-            function.workerVideo("test", 1)
-
-
-def test_workerVideoStream_4(function):
-    class Test:
-        @staticmethod
-        def isOpened():
-            return True
-
-        @staticmethod
-        def open(a):
-            return
-
-        @staticmethod
-        def grab():
-            function.running = False
-            return True
-
-        @staticmethod
-        def release():
-            return
-
-        @staticmethod
-        def setExceptionMode(a):
-            return
-
-    function.running = True
-    with mock.patch.object(cv2, "VideoCapture", return_value=Test()):
-        with mock.patch.object(function, "sendImage"):
-            function.workerVideo("test", 1)
-
-
-def test_startVideoStream_1(function):
-    function.user = "1"
-    function.password = "1"
-    function.ui.videoURL.setText("")
-    with mock.patch.object(function.threadPool, "start"):
-        function.startVideo()
-
-
-def test_startVideoStream_2(function):
-    function.user = ""
-    function.password = ""
-    function.ui.videoURL.setText("test")
-    with mock.patch.object(function.threadPool, "start"):
-        function.startVideo()
-
-
-def test_stopVideoStream_1(function):
-    function.stopVideo()
-
-
-def test_restartVideo(function):
-    with mock.patch.object(function, "stopVideo"):
-        with mock.patch.object(function, "startVideo"):
-            with mock.patch.object(gui.extWindows.video.videoW, "sleepAndEvents"):
-                function.restartVideo()
-
-
-def test_receivedImage_1(function):
-    pixmap = QPixmap(100, 100)
-    function.running = False
-    function.receivedImage(pixmap)
-
-
-def test_receivedImage_2(function):
-    pixmap = QPixmap(100, 100)
-    function.running = True
-    function.receivedImage(pixmap)
-
-
-def test_checkAuth(function):
-    function.checkAuth()
-
-
-def test_authPopup_1(function):
-    function.user = "test"
-    function.password = "test"
-    with mock.patch.object(function, "checkAuth"):
-        with mock.patch.object(function, "restartVideo"):
-            with mock.patch.object(QInputDialog, "getText", return_value=("test", False)):
-                function.authPopup()
-                assert function.user == "test"
-                assert function.password == "test"
-
-
-def test_authPopup_2(function):
-    function.user = "test"
-    function.password = "test"
-    with mock.patch.object(function, "checkAuth"):
-        with mock.patch.object(function, "restartVideo"):
-            with mock.patch.object(QInputDialog, "getText", return_value=("test1", True)):
-                function.authPopup()
-                assert function.user == "test1"
-                assert function.password == "test1"

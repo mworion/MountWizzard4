34f5f36971425b668f71840bebc6c22050758341
mworion <michael.wuertenberger@t-online.de>
2025-12-02 14:00:35 +0100

    corection

--- src/mw4/logic/buildData/buildpoints.py
+++ src/mw4/logic/buildData/buildpoints.py
@@ -9,7 +9,7 @@
 # Python-based Tool for interaction with the 10_micron mounts
 # GUI with PySide
 #
-# written in python3, (c) 2019-2025 by mworion
+# written in python3, (c) 2019-2025 by mworion 
 # Licence APL2.0
 #
 ###########################################################

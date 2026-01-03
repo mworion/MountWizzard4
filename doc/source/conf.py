ef5e2b51a0c4311bf4f77824dadd68cc42ed3c86
mworion <michael@wuertenberger.org>
2026-01-03 18:27:48 +0100

    fix tests

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -40,8 +40,8 @@ copyright = '2023, mworion'
 author = 'Michael Würtenberger'
 
 # The full version, including alpha/beta/rc tags
-version = '3.2.8.3b2'
-release = '3.2.8.3b2'
+version = '3.2.8.3b3'
+release = '3.2.8.3b3'
 master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ with open('notes.txt', 'w') as f:
 
 setup(
     name='mountwizzard4',
-    version='3.2.8.3b2',
+    version='3.2.8.3b3',
     packages=[
         'mw4',
         'mw4.base',

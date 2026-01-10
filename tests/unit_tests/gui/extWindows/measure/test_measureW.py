a9e72ce33680a7ab527d093c530b104782bac8b4
mworion <michael@wuertenberger.org>
2026-01-10 20:30:18 +0100

    fix test

--- tests/unit_tests/gui/extWindows/measure/test_measureW.py
+++ tests/unit_tests/gui/extWindows/measure/test_measureW.py
@@ -110,14 +110,14 @@ def test_setupButtons(function):
 
 def test_constructPlotItem_1(function):
     plotItem = pg.PlotItem()
-    values = dataPlots["Current"]
+    values = function.dataPlots["Current"]
     x = function.app.measure.data["time"].astype("datetime64[s]").astype("int")
     function.constructPlotItem(plotItem, values, x)
 
 
 def test_plotting_1(function):
     plotItem = pg.PlotItem()
-    values = dataPlots["Current"]
+    values = function.dataPlots["Current"]
     x = function.app.measure.data["time"].astype("datetime64[s]").astype("int")
     function.plotting(plotItem, values, x)
 
@@ -131,7 +131,7 @@ def test_resetPlotItem(function):
         return RemoveItem()
     plotItem = pg.PlotItem()
     plotItem.scene = scene
-    chart = dataPlots["Axis Stability"]
+    chart = function.dataPlots["Axis Stability"]
     function.resetPlotItem(plotItem, chart)
 
 

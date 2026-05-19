84e4d859845e3147f76d5666694063e8d299af73
mworion <michael.wuertenberger@t-online.de>
2026-05-19 14:01:57 +0200

    refactor nina / sgpro also for camera

---
+++ src/mw4/logic/camera/cameraSgproNinaBase.py
@@ -0,0 +1,109 @@
+############################################################
+#
+#       #   #  #   #   #    #
+#      ##  ##  #  ##  #    #
+#     # # # #  # # # #    #  #
+#    #  ##  #  ##  ##    ######
+#   #   #   #  #   #       #
+#
+# Python-based Tool for interaction with the 10_micron mounts
+# GUI with PySide
+#
+# written in python3, (c) 2019-2026 by mworion
+# License APL2.0
+#
+###########################################################
+from mw4.base.tpool import Worker
+from pathlib import Path
+from typing import Any
+
+
+class CameraSgproNinaBase:
+    def __init__(self, parent: Any) -> None:
+        super().__init__(parent=parent)
+        self.threadPool = parent.threadPool
+        self.worker: Worker | None = None
+
+    def workerGetInitialConfig(self) -> None:
+        self.storePropertyToData(1, "CCD_BINNING.HOR_BIN")
+
+    def sendDownloadMode(self) -> None:
+        pass
+
+    def waitFunc(self) -> bool:
+        return "integrating" in self.data.get("Device.Message")
+
+    def workerExpose(self) -> None:
+        params = {
+            "BinningMode": self.parent.binning,
+            "ExposureLength": max(self.parent.exposureTime, 1),
+            "Path": str(self.parent.imagePath),
+        }
+
+        suc, response = self.captureImage(params=params)
+        if not suc:
+            self.log.debug(f"No capture image. {response}")
+            return
+
+        receipt = response.get("Receipt", "")
+        if not receipt:
+            self.log.debug(f"No receipt received. {response}")
+            return
+
+        self.parent.waitStart()
+        self.parent.waitExposed(self.parent.exposureTime, self.waitFunc)
+        self.signals.exposed.emit(self.parent.imagePath)
+        self.parent.waitDownload()
+        self.signals.downloaded.emit(self.parent.imagePath)
+        self.parent.waitSave()
+        self.parent.waitFinish(self.getImagePath, receipt)
+
+        if not self.parent.exposing:
+            self.parent.imagePath = Path()
+        else:
+            self.parent.imagePath.with_suffix(".fit")
+            self.parent.updateImageFitsHeaderPointing()
+
+    def expose(self) -> None:
+        self.worker = Worker(self.workerExpose)
+        self.worker.signals.finished.connect(self.parent.exposeFinished)
+        self.threadPool.start(self.worker)
+
+    def abort(self) -> bool:
+        return self.abortImage()
+
+    def sendCoolerSwitch(self, coolerOn: bool = False) -> None:
+        pass
+
+    def sendCoolerTemp(self, temperature: float = 0) -> None:
+        pass
+
+    def sendOffset(self, offset: int = 0) -> None:
+        pass
+
+    def sendGain(self, gain: int = 0) -> None:
+        pass
+
+    def getCameraTemp(self) -> tuple[bool, dict]:
+        response = self.requestProperty("cameratemp") or {}
+        return response.get("Success", False), response
+
+    def setCameraTemp(self, temperature: float) -> bool:
+        response = self.requestProperty(f"setcameratemp/{temperature}")
+        return response.get("Success", False)
+
+    def captureImage(self, params: dict) -> tuple[bool, dict]:
+        response = self.requestProperty("image", params=params)
+        return response.get("Success", False), response
+
+    def abortImage(self) -> bool:
+        response = self.requestProperty("abortimage")
+        return response.get("Success", False)
+
+    def getImagePath(self, receipt: str) -> bool:
+        response = self.requestProperty(f"imagepath/{receipt}")
+        return response.get("Success", False)
+
+    def getCameraProps(self) -> tuple[bool, dict]:
+        response = self.requestProperty("cameraprops")
+        return response.get("Success", False), response
